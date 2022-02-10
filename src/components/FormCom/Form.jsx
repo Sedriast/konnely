6a246d896d3e3b8addc6b5d0ea404811dd2a1c67
JfAlonso <alonsofelipe8888@gmail.com>
2022-02-10 17:55:09 -0500

    ListType en funcionamiento

--- src/components/FormCom/Form.jsx
+++ src/components/FormCom/Form.jsx
@@ -3,8 +3,7 @@ import style3 from './css/aditionalData.module.css';
 import style2 from './css/basicData.module.css';
 import style from '../css/form.module.css';
 import { InputImage } from './InputImage';
-import { collection, onSnapshot } from "firebase/firestore";
-import { useEffect, useState } from 'react';
+import {  useState } from 'react';
 import { ListType } from './ListType';
 
 export function Form(props){
@@ -15,7 +14,6 @@ export function Form(props){
     }
 
 	const [values, setValues] = useState(init);
-	const [list, setList] = useState([{ name: "Loading...", id: "initial" }]);
 
 
 	const handleChanche = e => {
@@ -28,13 +26,6 @@ export function Form(props){
         props.AddOrEdit(values);
     }
 
-	useEffect(
-		() =>
-		  onSnapshot(collection(db, "raza"), (snapshot) =>
-			setList(snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id})))
-		  ),
-		[]);
-
 	/**
 	 const addInfo = async (Objeto) => {
 		try {
@@ -46,56 +37,23 @@ export function Form(props){
 		  }
 	}
 	 
-	function lista_2 () {
-		const lo = [];
-		JSON.stringify(list, replacer);
-
-		function replacer(key, value) {
-			// Filtrando propiedades 
-			if (typeof value === "string") {
-				lo.push(value);
-			}
-			return value;
-		  }
-		return lo;
-	}
-
-	
-	console.log(lista_2())
 	*/
-/*
-	function lista_3 () {
-		const lo = [ ];
-		list.map((color) => (
-			lo.push(color.name)
-			))
-		return lo
-	}
 
-	function id () {
-		const id = [];
-		list.map((color) => (
-			id.push(color.id)
-			))
-		
-		return id;
-	}
-*/
 	return(
 		<div className={style.inputsPanel}>
 			<form className={style2.basicDataPanel} onSubmit={handleSubmit}>
 				<input name= 'nombre' type='text' className={style.name} placeholder='Nombre' onChange={handleChanche}/>
 				<input type='date' className={style.dateBirth} /> 
 				<input type='date' className={style.dateDestete} />
-				<ListType k={list} />
+				<ListType collection='raza'/>
 				<input name= 'peso' type='text' className={style.weight} placeholder='Peso'onChange={handleChanche}/>
-				
+				<ListType collection='grupo'/>
 			</form>
-			<InputImage ty="date" pl="Nombre" />
+				<InputImage ty="date" pl="Nombre" />
 			<hr />
 			<form className={style3.addDataPanel} onSubmit={handleSubmit}>
-				<ListType k="Motivo del ingreso"/>
-				<ListType k="¿Donde?"/>
+				<ListType collection='motivo'/>
+				<ListType collection='donde'/>
 				<input name="Calificacion" placeholder='Calificacion' onChange={handleChanche}></input>
 				<input name="id" placeholder='ID'onChange={handleChanche}></input>
 			</form>
--- src/components/FormCom/ListType.jsx
+++ src/components/FormCom/ListType.jsx
@@ -1,49 +1,53 @@
-/**
- * Esta es la estructura basica y lista para modificar de una Lista desplegable
- * 
- */
+import db from '../firebase/credenciales';
+import style3 from './css/aditionalData.module.css';
+import style2 from './css/basicData.module.css';
+import { InputImage } from './InputImage';
+import { collection, onSnapshot } from "firebase/firestore";
+import { useEffect, useState } from 'react';
 
-import { useState } from 'react';
 import style from './css/basicData.module.css';
 
-export function ListType( props ){
+export function ListType(props){
 
-	const init = {
-        value:""
-    }
+	const coleccion = props.collection;
 
-	const s = props.k;
 
-	const [values, setValues] = useState(init);
+	const [values, setValues] = useState([{ name: "Loading..."}]);
+	const [list, setList] = useState([{ name: "Loading...", id: "initial" }]);
 
-	//Este vector maneja las opciones
-	const [op] = useState([props.k]);
-	  
+	useEffect(
+		() =>
+		  onSnapshot(collection(db, coleccion), (snapshot) =>
+			setList(snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id})))
+		  ),
+		[]);
+
+
+	function lista () {
+		const lo = [ ];
+		list.map((color) => (
+			lo.push(color.name)
+			))
+		return lo
+	}
+
+
 	const handleChange = e => {
 		const {name, value} = e.target;
-        setValues({...values, [name]:value})
+        setValues({[name]:value})
 	};
 	
 //Esta funcion se usa para enviar los datos seleccionados a donde te plasca
-
+	
 /** 
  	function handleSubmit (event) {
 		alert('selected is: ' + this.state);
 	}
 */
-function lista () {
-	const lo = [];
-	op.map((color) => (
-		lo.push(color.name)
-		))
-	return lo
-}
-
-console.log(lista())
 
 	return (
-		<select className={style.listTypes} value={init} onChange={handleChange} >
-			{ op.map(a=><option key={a.id} value={a.name}>{a.name}</option>)}
+		<select name="name" className={style.listTypes} onChange={handleChange} >
+			{ lista().map(a=><option key={a} value={a}>{a}</option>)}
 		</select>
 	);
 }
