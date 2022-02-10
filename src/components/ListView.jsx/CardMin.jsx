f47b01bfabbe7fb3df4bdf04210eb1afc982f67c
exSED <sedriast@gmail.com>
2022-02-10 15:51:15 -0500

    listStyle

---
+++ src/components/ListView.jsx/CardMin.jsx
@@ -0,0 +1,9 @@
+import style from '../css/listViews.module.css';
+
+export function CardMin(){
+    return(
+        <div className={style.cardPanel} >
+            
+        </div>
+    );
+}
--- src/components/ListView.jsx/ListViews.jsx
+++ src/components/ListView.jsx/ListViews.jsx
@@ -1,9 +1,10 @@
 import style from '../css/listViews.module.css';
+import { CardMin } from './CardMin';
 
 export function ListViews(){
     return(
         <div className={style.subPanel}>
-            
+            <CardMin />
 		</div>
     );
 }
