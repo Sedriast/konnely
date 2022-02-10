66f6193ba2c8b353aee307d74b7bb563ff1aa73d
exSED <sedriast@gmail.com>
2022-02-10 16:49:18 -0500

    Cards

--- src/components/ListView.jsx/CardMin.jsx
+++ src/components/ListView.jsx/CardMin.jsx
@@ -3,10 +3,18 @@ import style from '../css/listViews.module.css';
 export function CardMin(props){
 
     const url = props.url;
+    const rabitData = props.rabitData;
+
+    const test = () => {
+        window.alert("Good");
+    } 
 
     return(
-        <div className={style.cardPanel} >
-            <img className={style.imgView} src={url} />
-        </div>
+        <button onClick={test}>
+            <div className={style.cardPanel} >
+                <img className={style.imgView} src={url} />
+                <p>{rabitData}</p>
+            </div>
+        </button>
     );
 }
--- src/components/ListView.jsx/ListViews.jsx
+++ src/components/ListView.jsx/ListViews.jsx
@@ -1,10 +1,10 @@
 import style from '../css/listViews.module.css';
 import { CardMin } from './CardMin';
 
-export function ListViews(){
+export function ListViews(props){
     return(
         <div className={style.subPanel}>
-            <CardMin url='https://drive.google.com/uc?export=download&id=10dKfWHgApJ5ElbXCaOyrdguN8sac2jpA'/>
+            <CardMin url='https://drive.google.com/uc?export=download&id=10dKfWHgApJ5ElbXCaOyrdguN8sac2jpA' rabitData='Esto es una prueba bien pitera :V'/>
             <CardMin url='https://drive.google.com/uc?export=download&id=1HsBxcwNJEWm_4JcGQ2nFVsrnvcBTqtQD'/>
             <CardMin />
             <CardMin />
