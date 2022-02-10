9b3ece7d1a38098e87d335253f66a4d1478a68ee
exSED <sedriast@gmail.com>
2022-02-10 12:29:53 -0500

    Init II

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -5,12 +5,13 @@
 */
 
 import style from './css/base.module.css';
-import { Form } from './FormCom/Form';
+//import { Form } from './FormCom/Form';
+import { ListViews } from './ListView.jsx/ListViews';
 
 export function Layout(){
     return(
         <div className={style.principalPanel} >
-            <Form />
+            <ListViews />
         </div>
     );
 }
---
+++ src/components/ListView.jsx/ListViews.jsx
@@ -0,0 +1,9 @@
+import style from '../css/form.module.css';
+
+export function ListViews(){
+    return(
+        <div className={style.inputsPanel}>
+			
+		</div>
+    );
+}
