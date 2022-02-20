54ea99305c656bdf4da89b57cf364c0ebaa7a383
exSED <sedriast@gmail.com>
2022-02-20 13:47:58 -0500

    Corrections I

--- src/components/Searchbar/SearchBar.jsx
+++ src/components/Searchbar/SearchBar.jsx
@@ -5,8 +5,8 @@ export function SearchBar(props){
 	const clName = props.clName;
 	return(
 		<div className={clName}>
-			<div><input className={style.inputSe} type='text' placeholder='Buscar'/></div>
 			<div className={style.iconSePanel}><image className={style.iconSe} src='' href='' /></div>
+			<div><input className={style.inputSe} type='text' placeholder='Buscar'/></div>
 		</div>
 	); 
 }
