5fbf93453ef554f8b6f3e44ae18048c3cadc6db9
exSED <sedriast@gmail.com>
2022-02-23 19:49:38 -0500

    Corrections V

--- src/components/Buttons/Btn.jsx
+++ src/components/Buttons/Btn.jsx
@@ -1,9 +1,9 @@
 import { Link } from 'react-router-dom';
+import '../css/Btn.module.css'
 
 export function Btn(props){
 
     const clName = props.clName;
-    const name_ = props.name_B;
     const oClick_ = props.oClick_B;
     const oChange = props.oChangeB;
 
@@ -12,7 +12,7 @@ export function Btn(props){
 
     return(
     <>
-        <button name={name_} onClick={oClick_} onChange={oChange}>
+        <button onClick={oClick_} onChange={oChange}>
             <Link className={clName} to={itemPath}>
                 <h1>{text_}</h1>
             </Link>
