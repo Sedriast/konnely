fb4b3754357bbc5d1a2c500a4fba4ca98c3833ca
exSED <sedriast@gmail.com>
2022-03-04 18:35:19 -0500

    Init 2.0

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,27 +1,7 @@
-import style from './css/Layout.module.css';
-import { Navbar } from './NavbarCom/Navbar';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { Form } from './FormCom/Form';
-import { ListViews} from './ListView/ListViews';
-import { Login } from './Login/Login';
-import { UserView } from './UserView_/UserView';
-import { DataView } from './DataView/DataView';
-import { CardMin } from './ListView/CardMin';
 
-export function Layout() {
-	return (
-		<div className={style.principalPanel}>
-			<Router>
-				<Navbar />
-				<Routes >
-					<Route exact path='/form' element={<Form/>}> </Route>
-					<Route exact path='/listView' element={<ListViews idImage='10dKfWHgApJ5ElbXCaOyrdguN8sac2jpA'/>}> </Route>
-					<Route exact path='/login' element={<Login />}> </Route>
-					<Route exact path='/user' element={<UserView />}></Route>
-					<Route exact path='/data' element={<DataView />}></Route>
-					<Route exact path='/test' element={<CardMin />}></Route>
-				</Routes>
-			</Router>
-		</div>
-	);
+
+export function Layout(){
+    return(<>
+    
+    </>);
 }
