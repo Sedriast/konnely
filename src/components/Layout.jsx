96076cddf125444181c03c21b1c26ca932590582
exSED <sedriast@gmail.com>
2022-03-04 19:07:17 -0500

    Readme

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,11 @@
 import style_L from './css/Layout/Layout.module.css';
 
 export function Layout(){
-    return(<>
-        <div className={style_L.root_1}>
-            <div className={style_L.root_2}>
-
-            </div>
-        </div>
-    </>);
+	return(<>
+		<div className={style_L.root_1}>
+			<div className={style_L.root_2}>
+				
+			</div>
+		</div>
+	</>);
 }
