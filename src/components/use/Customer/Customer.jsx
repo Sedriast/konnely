e25f4a26810bbd126c1ed554b6288d3ffd05b5e8
exSED <sedriast@gmail.com>
2022-03-04 21:09:35 -0500

    Customer Init

--- src/components/use/Customer/Customer.jsx
+++ src/components/use/Customer/Customer.jsx
@@ -1,10 +1,13 @@
 import style_C from '../../css/Customer/Customer.module.css';
 
-export function Customer(){
+export function Customer(props){
+
+	const linkImg = props.src_;
+
 	return(
 		<>
 			<div className={style_C.Customer}>
-
+				<img src={linkImg} href='' alt=''/>
 			</div>
 		</>
 	);
