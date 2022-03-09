71bbe7999f86bf0ca08b487e8f7059250e195b02
exSED <sedriast@gmail.com>
2022-03-09 18:59:39 -0500

    List

--- src/components/use/List/List.jsx
+++ src/components/use/List/List.jsx
@@ -1,5 +1,6 @@
 import  style_Li from '../../css/List/List.module.css'
 import { Cards } from '../Tools/Cards';
+import { Inputs } from '../Tools/Inputs';
 
 export function List(props){
 
@@ -7,10 +8,13 @@ export function List(props){
 		<>
 			<div className={props.clsName}>
 				<div className={style_Li.panel_}>
-					{/* {data.map((a) => (
-					<Cards url={a.Objeto.url} rabitDataName={a.Objeto.nombre} 
-							data={a} data1={a.Objeto.raza} data2 ={a.Objeto.estado}/>
-					))} */}
+					<Inputs clsName={style_Li.searchBar} type_='text' placeholder_='Buscar' />
+					<div className={style_Li.grid}>					
+						{/* {data.map((a) => (
+						<Cards url={a.Objeto.url} rabitDataName={a.Objeto.nombre} 
+								data={a} data1={a.Objeto.raza} data2 ={a.Objeto.estado}/>
+						))} */}
+					</div>
 				</div>
 			</div>
 		</>
