0f0e05a0ea5edd52c98856674ae6bdbdfc1aca85
exSED <sedriast@gmail.com>
2022-03-10 18:41:01 -0500

    Errores

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,14 @@
 import { Layout } from "./components/Layout";
 import style_A from './components/css/App/App.module.css';
-
 import React from "react";
-const bk = "hola";
 
-const back = () => {
-
-	const v ='linear-gradient(0deg, rgba(236, 224, 216, 0.65), rgba(236, 224, 216, 0.65)), url(https://drive.google.com/uc?export=download&id=1rOlGY6t5QqpGusJE_TW4Z3HgOO2islVl)';
-
-	window.setInterval(function(){
-	document.getElementById('root').style.setProperty('background', v);
-	}, 1);
-
-	console.log(v+"		v");
-}
+const bk = "";
 
 export function App() {
 	return (
 		<>
 			<React.StrictMode>
-				<Layout clsName={style_A.Layout} cl_={back}/>
+				<Layout clsName={style_A.Layout} cl_={bk} cl2_={back}/>
 			</React.StrictMode>
 		</>
 	);
--- src/components/use/Customer/Data.js
+++ src/components/use/Customer/Data.js
@@ -2,31 +2,35 @@ export const Data = [
 	{
         name: "caryWhite",
         miniature: "https://drive.google.com/uc?export=download&id=1TfQRti17n6dtBIE0Jifb9L-88BFRFsjO",
-        backg: "https://drive.google.com/uc?export=download&id=1rOlGY6t5QqpGusJE_TW4Z3HgOO2islVl",
+        backgroud1:'linear-gradient(0deg, rgba(236, 224, 216, 0.65), rgba(236, 224, 216, 0.65)), url(https://drive.google.com/uc?export=download&id=1rOlGY6t5QqpGusJE_TW4Z3HgOO2islVl)',
 	},
 	{
         name: "darpa",
-        miniature: "",
-        backg: "",
+        miniature: "https://drive.google.com/uc?export=download&id=171YQUEx7BERY3MqrCQhk2mpR_Qwp7DLL",
+        backgroud2:'linear-gradient(0deg, rgba(236, 224, 216, 0.65), rgba(236, 224, 216, 0.65)), url(https://drive.google.com/uc?export=download&id=1rOlGY6t5QqpGusJE_TW4Z3HgOO2islVl)',
+
 	},	
     {
         name: "wine",
-        miniature: "",
-        backg: "",
+        miniature: "https://drive.google.com/uc?export=download&id=1lJhUinD1j3LSRRLtbXYR5V5k0Mz6dXnO",
+        backgroud3:'linear-gradient(0deg, rgba(236, 224, 216, 0.65), rgba(236, 224, 216, 0.65)), url(https://drive.google.com/uc?export=download&id=1rOlGY6t5QqpGusJE_TW4Z3HgOO2islVl)',
+
 	},	
     {
         name: "Broke",
-        miniature: "",
-        backg: "",
+        miniature: "https://drive.google.com/uc?export=download&id=1vAb2vSBiGQlzc_FQMFYGW0N0jbabRKw2",
+        backgroud4:'linear-gradient(0deg, rgba(236, 224, 216, 0.65), rgba(236, 224, 216, 0.65)), url(https://drive.google.com/uc?export=download&id=1rOlGY6t5QqpGusJE_TW4Z3HgOO2islVl)',
+
 	},	
     {
         name: "line",
-        miniature: "",
-        backg: "",
+        miniature: "https://drive.google.com/uc?export=download&id=1Fx25KV9ypYtAyzrfBeepao4rKoe6v1D3",
+        backgroud5:'linear-gradient(0deg, rgba(236, 224, 216, 0.65), rgba(236, 224, 216, 0.65)), url(https://drive.google.com/uc?export=download&id=1rOlGY6t5QqpGusJE_TW4Z3HgOO2islVl)',
+
 	},	
     {
         name: "caryBlack",
         miniature: "https://drive.google.com/uc?export=download&id=11aszYhyH-Hq0-mq1QIo-t9cLYCl88TnE",
-        backg: 'https://drive.google.com/uc?export=download&id=1rOlGY6t5QqpGusJE_TW4Z3HgOO2islVl',
+        backgroud6:'linear-gradient(0deg, rgba(24, 3, 1, 0.65), rgba(24, 3, 1, 0.65)), url(https://drive.google.com/uc?export=download&id=1rOlGY6t5QqpGusJE_TW4Z3HgOO2islVl)',
 	},
 ];
