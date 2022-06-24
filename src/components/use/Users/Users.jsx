007eebd3b4e5f0a5942f85c22cb231248216192e
exSED <sedriast@gmail.com>
2022-06-24 16:40:59 -0500

    User init

--- src/components/use/Users/Users.jsx
+++ src/components/use/Users/Users.jsx
@@ -1,22 +1,28 @@
-import style_U from '../../css/Users/Users.module.css';
+import st from './css/Users.module.css';
 import { Buttons } from '../Tools/Buttons/Buttons';
 
 export function Users(props) {
 	return (
 		<>
-			<div className={props.clsName}>
-				<div className={style_U.panel_}>
-					<div className={style_U.panelImg_}>
-						<img className={style_U.userImg_} src={props.src_} alt="" href="" />
-						<Buttons clsName={style_U.op1} link_="/" />
-						<Buttons clsName={style_U.op2} link_="/" />
-						<Buttons clsName={style_U.op3} link_="/" />
-						<Buttons clsName={style_U.op4} link_="/" />
-					</div>
-					<p>
-						<h1>{props.title}</h1>
-						<h3>{props.label}</h3>
-					</p>
+			<div className={st.container}>
+				<div className={st.b}>
+					<Buttons link_="/customer" />
+				</div>
+				<div className={st.b}>
+					<Buttons link_="/" />
+				</div>
+				<div className={st.b}>
+					<Buttons link_="/" />
+				</div>
+				<div className={st.b}>
+					<Buttons link_="/" />
+				</div>
+				<p>
+					<h1>{props.title}</h1>
+					<h3>{props.label}</h3>
+				</p>
+				<div className={st.im}>
+					<img src={props.src_} alt="" href="" />
 				</div>
 			</div>
 		</>
