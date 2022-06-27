0549eba9995d8847a82256dde24a1d302d89e3e9
exSED <sedriast@gmail.com>
2022-06-27 13:30:07 -0500

    Customer

--- src/components/use/Customer/Customer.jsx
+++ src/components/use/Customer/Customer.jsx
@@ -1,57 +1,55 @@
-import style_Cu from "../../css/Customer/Customer.module.css";
-import { Data } from "./Data.js";
-import { useState } from "react";
-import { UpdateInfoProfile } from "../../firebase/funtions/AddInformation";
-import { useAuth } from "../../../context/AuthContext";
-import swal from "sweetalert";
+import st from './css/Customer.module.css';
+import { Data } from './Data.js';
+import { useState } from 'react';
+import { UpdateInfoProfile } from '../../firebase/funtions/AddInformation';
+import { useAuth } from '../../../context/AuthContext';
+import swal from 'sweetalert';
 
 export function Customer(props) {
-    const { user } = useAuth();
-    const [f, setF] = useState({ user: null, data: {} });
+	const { user } = useAuth();
+	const [f, setF] = useState({ user: null, data: {} });
 
-    const ca = (e) => {
-        setF({ ...f, user: user.uid, data: { tema: e.target.id } });
-    };
+	const ca = (e) => {
+		setF({ ...f, user: user.uid, data: { tema: e.target.id } });
+	};
 
-    const c = () => {
-        swal({
-            title: "¿Desea cambiar el tema predeterminado?",
-            icon: "warning",
-            buttons: ["No", "Si"],
-        }).then((respuesta) => {
-            if (respuesta) {
-                UpdateInfoProfile(f);
-                setTimeout(recargar, 1000);
-            }
-        });
-    };
+	const c = () => {
+		swal({
+			title: '¿Desea cambiar el tema predeterminado?',
+			icon: 'warning',
+			buttons: ['No', 'Si'],
+		}).then((respuesta) => {
+			if (respuesta) {
+				UpdateInfoProfile(f);
+				setTimeout(recargar, 1000);
+			}
+		});
+	};
 
-    function recargar() {
-        window.location.reload(true);
-    }
-    return (
-        <>
-            <div className={props.clsName}>
-                <div className={style_Cu.panel_}>
-                    {Data.map((a, index) => {
-                        return (
-                            <div key={index} className={style_Cu.theme}>
-                                <button className={style_Cu.ch} onMouseEnter={ca}>
-                                    <img
-                                        className={style_Cu.preview_}
-                                        id={a.backgroud}
-                                        src={a.miniature_1}
-                                        // src={require(a.miniature).default}
-                                        onClick={c}
-                                        href=""
-                                        alt=""
-                                    />
-                                </button>
-                            </div>
-                        );
-                    })}
-                </div>
-            </div>
-        </>
-    );
+	function recargar() {
+		window.location.reload(true);
+	}
+	return (
+		<>
+			<div className={st.container}>
+				{Data.map((a, index) => {
+					return (
+						<div className={st.ops} key={index}>
+							<button onMouseEnter={ca}>
+								<img
+									className={st.preview_}
+									id={a.backgroud}
+									src={a.miniature_1}
+									// src={require(a.miniature).default}
+									onClick={c}
+									href=""
+									alt=""
+								/>
+							</button>
+						</div>
+					);
+				})}
+			</div>
+		</>
+	);
 }
--- src/components/use/Customer/Data.js
+++ src/components/use/Customer/Data.js
@@ -1,57 +1,51 @@
-import uno_ from "../../css/Img/1_.png";
-import dos_ from "../../css/Img/2_.png";
-import tres_ from "../../css/Img/3_.png";
-import cuatro_ from "../../css/Img/4_.png";
-import cinco_ from "../../css/Img/5_.png";
-import seis_ from "../../css/Img/6_.png";
+import uno_ from '../../img/backgrounds/0.png';
+import dos_ from '../../img/backgrounds/1.png';
+import tres_ from '../../img/backgrounds/2.png';
+import cuatro_ from '../../img/backgrounds/3.png';
+import cinco_ from '../../img/backgrounds/4.png';
+import seis_ from '../../img/backgrounds/5.png';
 
 export const Data = [
-    {
-        name: "caryWhite",
-        miniature: uno_,
-        miniature_1:
-            "https://drive.google.com/uc?export=download&id=1TfQRti17n6dtBIE0Jifb9L-88BFRFsjO",
-        backgroud:
-            "url(https://drive.google.com/uc?export=download&id=1p-s71I4bnyv5Gid9Iv6VzL6QEi-0iqba)",
-    },
-    {
-        name: "darpa",
-        miniature: dos_,
-        miniature_1:
-            "https://drive.google.com/uc?export=download&id=171YQUEx7BERY3MqrCQhk2mpR_Qwp7DLL",
-        backgroud:
-            "url(https://drive.google.com/uc?export=download&id=1bqq3el_cZUMSzOvs9OyBW5UakjNES9Iv)",
-    },
-    {
-        name: "wine",
-        miniature: tres_,
-        miniature_1:
-            "https://drive.google.com/uc?export=download&id=1lJhUinD1j3LSRRLtbXYR5V5k0Mz6dXnO",
-        backgroud:
-            "url(https://drive.google.com/uc?export=download&id=1IUHu6CRZWVMQAolKFL8X-qqtdkJUc8je)",
-    },
-    {
-        name: "Broke",
-        miniature: cuatro_,
-        miniature_1:
-            "https://drive.google.com/uc?export=download&id=1vAb2vSBiGQlzc_FQMFYGW0N0jbabRKw2",
-        backgroud:
-            "url(https://drive.google.com/uc?export=download&id=1XoiOIa2O2gJXpzy9wKGMvBdgMWZYJ6k2)",
-    },
-    {
-        name: "line",
-        miniature: cinco_,
-        miniature_1:
-            "https://drive.google.com/uc?export=download&id=1udAanIgzMRTlXu3V7YNtTAP0X7L5hG59",
-        backgroud:
-            "url(https://drive.google.com/uc?export=download&id=1KngiqrQ-IA8sekHOcDq_0FZ7ne4W-SeR)",
-    },
-    {
-        name: "caryBlack",
-        miniature: seis_,
-        miniature_1:
-            "https://drive.google.com/uc?export=download&id=11aszYhyH-Hq0-mq1QIo-t9cLYCl88TnE",
-        backgroud:
-            "url(https://drive.google.com/uc?export=download&id=1uD5Xb_2enNKu4rzR3FXhq-IePyMGlQ4H)",
-    },
+	{
+		name: 'caryWhite',
+		miniature: uno_,
+		miniature_1: 'https://drive.google.com/uc?export=download&id=1TfQRti17n6dtBIE0Jifb9L-88BFRFsjO',
+		backgroud:
+			'url(https://drive.google.com/uc?export=download&id=1p-s71I4bnyv5Gid9Iv6VzL6QEi-0iqba)',
+	},
+	{
+		name: 'darpa',
+		miniature: dos_,
+		miniature_1: 'https://drive.google.com/uc?export=download&id=171YQUEx7BERY3MqrCQhk2mpR_Qwp7DLL',
+		backgroud:
+			'url(https://drive.google.com/uc?export=download&id=1bqq3el_cZUMSzOvs9OyBW5UakjNES9Iv)',
+	},
+	{
+		name: 'wine',
+		miniature: tres_,
+		miniature_1: 'https://drive.google.com/uc?export=download&id=1lJhUinD1j3LSRRLtbXYR5V5k0Mz6dXnO',
+		backgroud:
+			'url(https://drive.google.com/uc?export=download&id=1IUHu6CRZWVMQAolKFL8X-qqtdkJUc8je)',
+	},
+	{
+		name: 'Broke',
+		miniature: cuatro_,
+		miniature_1: 'https://drive.google.com/uc?export=download&id=1vAb2vSBiGQlzc_FQMFYGW0N0jbabRKw2',
+		backgroud:
+			'url(https://drive.google.com/uc?export=download&id=1XoiOIa2O2gJXpzy9wKGMvBdgMWZYJ6k2)',
+	},
+	{
+		name: 'line',
+		miniature: cinco_,
+		miniature_1: 'https://drive.google.com/uc?export=download&id=1udAanIgzMRTlXu3V7YNtTAP0X7L5hG59',
+		backgroud:
+			'url(https://drive.google.com/uc?export=download&id=1KngiqrQ-IA8sekHOcDq_0FZ7ne4W-SeR)',
+	},
+	{
+		name: 'caryBlack',
+		miniature: seis_,
+		miniature_1: 'https://drive.google.com/uc?export=download&id=11aszYhyH-Hq0-mq1QIo-t9cLYCl88TnE',
+		backgroud:
+			'url(https://drive.google.com/uc?export=download&id=1uD5Xb_2enNKu4rzR3FXhq-IePyMGlQ4H)',
+	},
 ];
