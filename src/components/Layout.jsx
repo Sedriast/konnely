2244902c5c4cb24f4a9a2cabd96270fe21dd9422
exSED <sedriast@gmail.com>
2022-07-05 14:50:43 -0500

    lvl1

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,166 +1,151 @@
-import st from "./Layout.module.css";
-import app from "../components/firebase/credentials";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import st from './Layout.module.css';
+import app from '../components/firebase/credentials';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-import { Init } from "./use/Init/Init";
-import { List } from "./use/List/List";
-import { Form } from "./use/Form/Form";
-import { useState, useEffect } from "react";
-import { Invoice } from "./use/Invoice/Invoice";
-import { useAuth } from "../context/AuthContext";
-import { Calendar } from "./use/Calendar/Calendar";
-import { Customer } from "./use/Customer/Customer";
-import { Users } from "../components/use/Users/Users";
-import { PanelButtons } from "./use/PanelButons/PanelButtons";
-import { ProtectedRoute } from "./protectedRoute/ProtectedRoute";
-import {
-    collection,
-    getFirestore,
-    onSnapshot,
-    query,
-    where,
-} from "firebase/firestore";
+import { Init } from './use/Init/Init';
+import { List } from './use/List/List';
+import { Form } from './use/Form/Form';
+import { useState, useEffect } from 'react';
+import { Invoice } from './use/Invoice/Invoice';
+import { useAuth } from '../context/AuthContext';
+import { Calendar } from './use/Calendar/Calendar';
+import { Customer } from './use/Customer/Customer';
+import { Users } from '../components/use/Users/Users';
+import { PanelButtons } from './use/PanelButons/PanelButtons';
+import { ProtectedRoute } from './protectedRoute/ProtectedRoute';
+import { collection, getFirestore, onSnapshot, query, where } from 'firebase/firestore';
 
 const db = getFirestore(app);
 
 export function Layout() {
-    const { user } = useAuth();
-    const [user_, setUser_] = useState([
-        {
-            tema: "",
-            usuario: "",
-            email: "",
-            rol: "",
-            uid: "",
-            foto: "",
-        },
-    ]);
+	const { user } = useAuth();
+	const [user_, setUser_] = useState([
+		{
+			tema: '',
+			usuario: '',
+			email: '',
+			rol: '',
+			uid: '',
+			foto: '',
+		},
+	]);
 
-    useEffect(() => {
-        if (user) {
-            const q = query(
-                collection(db, "usuarios"),
-                where("uid", "==", user.uid)
-            );
-            onSnapshot(q, (snapshot) =>
-                setUser_(snapshot.docs.map((doc) => ({ ...doc.data() })))
-            );
-        }
-    }, [user]);
+	useEffect(() => {
+		if (user) {
+			const q = query(collection(db, 'usuarios'), where('uid', '==', user.uid));
+			onSnapshot(q, (snapshot) => setUser_(snapshot.docs.map((doc) => ({ ...doc.data() }))));
+		}
+	}, [user]);
 
-    const s = () => {
-        document
-            .getElementById("lay")
-            .style.setProperty("background", user_[0].tema);
-        document
-            .getElementById("lay")
-            .style.setProperty("background-repeat", "no-repeat");
-        document.getElementById("lay").style.setProperty("background-size", "cover");
-    };
+	const s = (u) => {
+		document.getElementById('lay').style.setProperty('background', 'url(' + u[0].tema + ')');
+		document.getElementById('lay').style.setProperty('background-repeat', 'no-repeat');
+		document.getElementById('lay').style.setProperty('background-size', 'cover');
+	};
 
-    return (
-        <>
-            <div className={st.container} id="lay" onLoad={s}>
-                <Router>
-                    <Routes>
-                        <Route
-                            exact
-                            path="/home"
-                            element={
-                                <>
-                                    <ProtectedRoute>
-                                        <PanelButtons />
-                                        <Calendar />
-                                    </ProtectedRoute>
-                                </>
-                            }
-                        />
-                        <Route
-                            exact
-                            path="/"
-                            element={
-                                <div className={st.initPanel}>
-                                    <Init />
-                                </div>
-                            }
-                        />
-                        <Route
-                            exact
-                            path="/customer"
-                            element={
-                                <>
-                                    <ProtectedRoute>
-                                        <Customer clsName={st.customer} />
-                                    </ProtectedRoute>
-                                </>
-                            }
-                        />
-                        <Route
-                            exact
-                            path="/users"
-                            element={
-                                <>
-                                    <ProtectedRoute>
-                                        <Users
-                                            clsName={st.users}
-                                            src_={user_[0].foto}
-                                            title={user_[0].usuario}
-                                            label="Adminitrador"
-                                        />
-                                    </ProtectedRoute>
-                                </>
-                            }
-                        />
-                        <Route
-                            exact
-                            path="/form"
-                            element={
-                                <>
-                                    <ProtectedRoute>
-                                        <PanelButtons />
-                                        <div className={st.form}>
-                                            <Form />
-                                        </div>
-                                    </ProtectedRoute>
-                                </>
-                            }
-                        />
-                        <Route
-                            exact
-                            path="/list"
-                            element={
-                                <>
-                                    <ProtectedRoute>
-                                        <PanelButtons />
-                                        <div className={st.list}>
-                                            <List />
-                                        </div>
-                                    </ProtectedRoute>
-                                </>
-                            }
-                        />
-                        <Route
-                            exact
-                            path="/invoice"
-                            element={
-                                <>
-                                    <ProtectedRoute>
-                                        <PanelButtons />
-                                        <Invoice
-                                            clsName={st.invoice}
-                                            name="sadfadf"
-                                            nit="safasdfa"
-                                            email="sadfads@asdfasdfaf.com"
-                                            date="asdfasdf"
-                                            userID="asdfassdfad"
-                                        />
-                                    </ProtectedRoute>
-                                </>
-                            }
-                        />
-                    </Routes>
-                </Router>
-            </div>
-        </>
-    );
+	return (
+		<>
+			<div className={st.container} id="lay" onLoad={() => s(user_)}>
+				<Router>
+					<Routes>
+						<Route
+							exact
+							path="/home"
+							element={
+								<>
+									<ProtectedRoute>
+										<PanelButtons />
+										<Calendar />
+									</ProtectedRoute>
+								</>
+							}
+						/>
+						<Route
+							exact
+							path="/"
+							element={
+								<div className={st.initPanel}>
+									<Init />
+								</div>
+							}
+						/>
+						<Route
+							exact
+							path="/customer"
+							element={
+								<>
+									<ProtectedRoute>
+										<Customer clsName={st.customer} />
+									</ProtectedRoute>
+								</>
+							}
+						/>
+						<Route
+							exact
+							path="/users"
+							element={
+								<>
+									<ProtectedRoute>
+										<Users
+											clsName={st.users}
+											src_={user_[0].foto}
+											title={user_[0].usuario}
+											label="Adminitrador"
+										/>
+									</ProtectedRoute>
+								</>
+							}
+						/>
+						<Route
+							exact
+							path="/form"
+							element={
+								<>
+									<ProtectedRoute>
+										<PanelButtons />
+										<div className={st.form}>
+											<Form />
+										</div>
+									</ProtectedRoute>
+								</>
+							}
+						/>
+						<Route
+							exact
+							path="/list"
+							element={
+								<>
+									<ProtectedRoute>
+										<PanelButtons />
+										<div className={st.list}>
+											<List />
+										</div>
+									</ProtectedRoute>
+								</>
+							}
+						/>
+						<Route
+							exact
+							path="/invoice"
+							element={
+								<>
+									<ProtectedRoute>
+										<PanelButtons />
+										<Invoice
+											clsName={st.invoice}
+											name="sadfadf"
+											nit="safasdfa"
+											email="sadfads@asdfasdfaf.com"
+											date="asdfasdf"
+											userID="asdfassdfad"
+										/>
+									</ProtectedRoute>
+								</>
+							}
+						/>
+					</Routes>
+				</Router>
+			</div>
+		</>
+	);
 }
--- src/components/use/Calendar/Calendar.jsx
+++ src/components/use/Calendar/Calendar.jsx
@@ -46,70 +46,53 @@ export function Calendar() {
 
 	const setNewDate = () => {
 		currentDate.setFullYear(currentYear, monthNumber, currentDay);
-		//	writeMonth(monthNumber);
+		writeMonth(monthNumber);
+	};
+	const writeMonth = (month) => {
+		for (let i = startDay(); i > 0; i--) {
+			<div class="calendar__date calendar__item calendar__last-days">
+				${getTotalDays(monthNumber - 1) - (i - 1)}
+			</div>;
+		}
+
+		for (let i = 1; i <= getTotalDays(month); i++) {
+			if (i === currentDay) {
+				return <div class="calendar__date calendar__item calendar__today">${i}</div>;
+			} else {
+				return <div class="calendar__date calendar__item">${i}</div>;
+			}
+		}
+	};
+	const getTotalDays = (month) => {
+		if (month === -1) month = 11;
+
+		if (
+			month === 0 ||
+			month === 2 ||
+			month === 4 ||
+			month === 6 ||
+			month === 7 ||
+			month === 9 ||
+			month === 11
+		) {
+			return 31;
+		} else if (month === 3 || month === 5 || month === 8 || month === 10) {
+			return 30;
+		} else {
+			return isLeap() ? 29 : 28;
+		}
 	};
 
-	// var dates = document.getElementById('dates');
-	// var month = document.getElementById('month');
-	// var year = document.getElementById('year');
+	const isLeap = () => {
+		return (currentYear % 100 !== 0 && currentYear % 4 === 0) || currentYear % 400 === 0;
+	};
 
-	// var prevMonthDOM = document.getElementById('prev-month');
-	// var nextMonthDOM = document.getElementById('next-month');
+	const startDay = () => {
+		let start = new Date(currentYear, monthNumber, 1);
+		return start.getDay() - 1 === -1 ? 6 : start.getDay() - 1;
+	};
 
-	// month.textContent = monthNames[monthNumber];
-	// year.textContent = currentYear.toString();
-
-	// prevMonthDOM.addEventListener('click', () => lastMonth());
-	// nextMonthDOM.addEventListener('click', () => nextMonth());
-
-	// const writeMonth = (month) => {
-	// 	for (let i = startDay(); i > 0; i--) {
-	// 		dates.innerHTML += (
-	// 			<div class="calendar__date calendar__item calendar__last-days">
-	// 				${getTotalDays(monthNumber - 1) - (i - 1)}
-	// 			</div>
-	// 		);
-	// 	}
-
-	// 	for (let i = 1; i <= getTotalDays(month); i++) {
-	// 		if (i === currentDay) {
-	// 			dates.innerHTML += <div class="calendar__date calendar__item calendar__today">${i}</div>;
-	// 		} else {
-	// 			dates.innerHTML += <div class="calendar__date calendar__item">${i}</div>;
-	// 		}
-	// 	}
-	// };
-
-	// const getTotalDays = (month) => {
-	// 	if (month === -1) month = 11;
-
-	// 	if (
-	// 		month == 0 ||
-	// 		month == 2 ||
-	// 		month == 4 ||
-	// 		month == 6 ||
-	// 		month == 7 ||
-	// 		month == 9 ||
-	// 		month == 11
-	// 	) {
-	// 		return 31;
-	// 	} else if (month == 3 || month == 5 || month == 8 || month == 10) {
-	// 		return 30;
-	// 	} else {
-	// 		return isLeap() ? 29 : 28;
-	// 	}
-	// };
-
-	// const isLeap = () => {
-	// 	return (currentYear % 100 !== 0 && currentYear % 4 === 0) || currentYear % 400 === 0;
-	// };
-
-	// const startDay = () => {
-	// 	let start = new Date(currentYear, monthNumber, 1);
-	// 	return start.getDay() - 1 === -1 ? 6 : start.getDay() - 1;
-	// };
-
-	// writeMonth(monthNumber);
+	writeMonth(monthNumber);
 
 	return (
 		<>
