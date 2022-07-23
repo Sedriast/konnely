c61ff5f872e7d900d759efbde19c6732b815f0f7
Jfalonso <alonsofelipe8888@gmail.com>
2022-07-22 21:07:25 -0500

    React.StrictMode:Ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
-import React from 'react';
 import st from './App.module.css';
+
 import { Layout } from './components/Layout';
 import { AuthProvider } from './context/AuthContext';
 
 export function App() {
     return (
         <>
-            <React.StrictMode>
-                <AuthProvider>
-                    <Layout clsName={st.Layout} />
-                </AuthProvider>
-            </React.StrictMode>
+            <AuthProvider>
+                <Layout clsName={st.Layout} />
+            </AuthProvider>
         </>
     );
 }
