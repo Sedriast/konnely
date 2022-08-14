4b0d10628d290f724abb0ca3f5aded918afee205
exSED <sedriast@gmail.com>
2022-08-14 13:12:52 -0500

    14-08

--- src/components/use/Tools/Cards/FormCR.jsx
+++ src/components/use/Tools/Cards/FormCR.jsx
@@ -1,5 +1,30 @@
+import { Inputs } from '../Inputs/Inputs';
 import st from './css/FormCR.module.css';
 
-export function FormCR() {
-	return <></>;
+export function FormCR(props) {
+	function handleChange() {}
+	return (
+		<>
+			<div className={st.container}>
+				<div className={st.panel}>
+					<h2>Title</h2>
+					{/* <h2>{props.title}</h2> */}
+					<Inputs
+						leyend="Origen"
+						name_="origen"
+						placeholder_="Ingrese el origen"
+						type_="text"
+						handleChange={handleChange}
+					/>
+					<Inputs
+						leyend="Origen"
+						name_="origen"
+						placeholder_="Ingrese el origen"
+						type_="text"
+						handleChange={handleChange}
+					/>
+				</div>
+			</div>
+		</>
+	);
 }
--- src/components/use/Vitae/Vitae.jsx
+++ src/components/use/Vitae/Vitae.jsx
@@ -9,98 +9,100 @@ import { Buttons } from '../Tools/Buttons/Buttons';
 import { QueriesSimple_ } from '../../firebase/funtions/QueriesSimple_';
 
 export function Vitae() {
-    const navigate = useNavigate();
+	const navigate = useNavigate();
 
-    function rabbitData() {
-        if (basicData.id === null) {
-            return null;
-        } else {
-            return QueriesSimple_({
-                coleccion: 'conejos',
-                parametro: 'id',
-                busqueda: basicData.id,
-            }).props.children[0];
-        }
-    }
-    useEffect(() => {
-        if (basicData.id === null) {
-            navigate('/list');
-        }
-    }, [navigate]);
+	function rabbitData() {
+		if (basicData.id === null) {
+			return null;
+		} else {
+			return QueriesSimple_({
+				coleccion: 'conejos',
+				parametro: 'id',
+				busqueda: basicData.id,
+			}).props.children[0];
+		}
+	}
+	useEffect(() => {
+		if (basicData.id === null) {
+			navigate('/list');
+		}
+	}, [navigate]);
 
-    return (
-        <>
-            <div className={st.container}>
-                <div className={st.panelBasicInfo}>
-                    {basicData.id !== null ? (
-                        <>
-                            <div className={st.imgVitae}>.............................................</div>
-                            <div className={st.basicInf}>
-                                <div className={st.re}>
-                                    <div className={st.id}>
-                                        <h1>Identificador</h1>
-                                        <div className={st.viewI}>{rabbitData().id}</div>
-                                    </div>
-                                    <div className={st.raza}>
-                                        <h1>Raza</h1>
-                                        <div className={st.viewI}>{rabbitData().raza}</div>
-                                    </div>
-                                    <div className={st.genero}>
-                                        <h1>Genero</h1>
-                                        <div className={st.viewI}>{rabbitData().genero}</div>
-                                    </div>
-                                </div>
-                                <div className={st.re}>
-                                    <div className={st.idPadre}>
-                                        <h1>Id. Padre</h1>
-                                        <div className={st.viewI}>{rabbitData().idPadre}</div>
-                                    </div>
-                                    <div className={st.idMadre}>
-                                        <h1>id. Madre</h1>
-                                        <div className={st.viewI}>{rabbitData().idMadre}</div>
-                                    </div>
-                                    <div className={st.origen}>
-                                        <h1>Origen</h1>
-                                        <div className={st.viewI}>{rabbitData().origen}</div>
-                                    </div>
-                                </div>
-                                <div className={st.re}>
-                                    <div className={st.time1}>
-                                        <h1>Fecha concepcion</h1>
-                                        <div className={st.viewI}>{rabbitData().nacimiento}</div>
-                                    </div>
-                                    <div className={st.procentaje}>
-                                        <h1>Porcentaje pureza</h1>
-                                        <div className={st.viewI}>{rabbitData().porcentaje}</div>
-                                    </div>
-                                </div>
-                            </div>
-                        </>
-                    ) : (
-                        <></>
-                    )}
-                </div>
+	return (
+		<>
+			<div className={st.container}>
+				<div className={st.panelBasicInfo}>
+					{basicData.id !== null ? (
+						<>
+							<div className={st.imgVitae}>
+								<img src="{}" alt="" />
+							</div>
+							<div className={st.basicInf}>
+								<div className={st.re}>
+									<div className={st.id}>
+										<h1>Identificador</h1>
+										<div className={st.viewI}>{rabbitData().id}</div>
+									</div>
+									<div className={st.raza}>
+										<h1>Raza</h1>
+										<div className={st.viewI}>{rabbitData().raza}</div>
+									</div>
+									<div className={st.genero}>
+										<h1>Genero</h1>
+										<div className={st.viewI}>{rabbitData().genero}</div>
+									</div>
+								</div>
+								<div className={st.re}>
+									<div className={st.idPadre}>
+										<h1>Id. Padre</h1>
+										<div className={st.viewI}>{rabbitData().idPadre}</div>
+									</div>
+									<div className={st.idMadre}>
+										<h1>id. Madre</h1>
+										<div className={st.viewI}>{rabbitData().idMadre}</div>
+									</div>
+									<div className={st.origen}>
+										<h1>Origen</h1>
+										<div className={st.viewI}>{rabbitData().origen}</div>
+									</div>
+								</div>
+								<div className={st.re}>
+									<div className={st.time1}>
+										<h1>Fecha concepcion</h1>
+										<div className={st.viewI}>{rabbitData().nacimiento}</div>
+									</div>
+									<div className={st.procentaje}>
+										<h1>Porcentaje pureza</h1>
+										<div className={st.viewI}>{rabbitData().porcentaje}</div>
+									</div>
+								</div>
+							</div>
+						</>
+					) : (
+						<></>
+					)}
+				</div>
 
-                <Lifecycle id={rabbitData()} />
+				<Lifecycle id={rabbitData()} />
 
-                <div className={st.stadistics}>
-                    <div className={st.rej}>Crias rechasadas: {reproData.rechazos}</div>
-                    <div className={st.dea}>Crias muertas: {reproData.muertes}</div>
-                    <div className={st.lif}>Crias vivas: {reproData.vivos}</div>
-                </div>
+				<div className={st.stadistics}>
+					<div className={st.rej}>Crias rechasadas: {reproData.rechazos}</div>
+					<div className={st.dea}>Crias muertas: {reproData.muertes}</div>
+					<div className={st.lif}>Crias vivas: {reproData.vivos}</div>
+				</div>
 
-                <div className={st.btnEdits}>
-                    <div className={st.basicData}>
-                        <Buttons link_='/form' text_='Datos básicos' icon_={ed} />
-                    </div>
-                    <div className={st.lifeCi}>
-                        <Buttons link_='/list' text_='Ciclo de vida' icon_={ed} />
-                    </div>
-                    <div className={st.nPartos}>
-                        <Buttons link_='#'>{reproData.partos}</Buttons>
-                    </div>
-                </div>
-            </div>
-        </>
-    );
+				<div className={st.btnEdits}>
+					<div className={st.basicData}>
+						<Buttons link_="/form" text_="Datos básicos" icon_={ed} />
+					</div>
+					<div className={st.lifeCi}>
+						<Buttons link_="/list" text_="Ciclo de vida" icon_={ed} />
+					</div>
+					<div className={st.nPartos}>
+						<Buttons link_="#">{reproData.partos}</Buttons>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 }
