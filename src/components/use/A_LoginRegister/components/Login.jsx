3eebf485ab0bf64f3b45d1a3a8b08d8834794aaa
JfAlonso <alonsofelipe8888@gmail.com>
2022-08-19 14:47:18 -0500

    LoginSinEstilos

--- src/components/use/A_LoginRegister/components/Login.jsx
+++ src/components/use/A_LoginRegister/components/Login.jsx
@@ -1,60 +1,49 @@
 import st from '../styles/Login.module.css';
-import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-import { Inputs } from '../../Tools/Inputs/Inputs';
-import { Buttons } from '../../Tools/Buttons/Buttons';
-import { ValidationErrors } from '../scripts/ValidationErrors';
 import { useAuth } from '../../../../context/AuthContext';
+import { ValidationErrors } from '../scripts/ValidationErrors';
 
 export function Login() {
-	const { login } = useAuth();
+    const { login } = useAuth();
+    const navigate = useNavigate();
 
-	const [user, setUser] = useState({
-		email: '',
-		password: '',
-	});
+    const handleSubmit = async (e) => {
+        try {
+            await login(e.email, e.password);
+            navigate('/list');
+        } catch (error) {
+            ValidationErrors(error.code);
+        }
+    };
 
-	const navigate = useNavigate();
-
-	function handleChange(e) {
-		const { name, value } = e.target;
-		setUser({ ...user, [name]: value });
-	}
-
-	const handleSubmit = async (e) => {
-		e.preventDefault();
-		try {
-			await login(user.email, user.password);
-			navigate('/list');
-		} catch (error) {
-			ValidationErrors(error.code);
-		}
-	};
-
-	return (
-		<>
-			<div className={st.container}>
-				<div className={st.inpCont}>
-					<Inputs
-						name_="email"
-						type_="email"
-						leyend="Correo electrónico"
-						handleChange={handleChange}
-					/>
-				</div>
-				<div className={st.inpCont}>
-					<Inputs
-						name_="password"
-						type_="password"
-						leyend="Contraseña"
-						handleChange={handleChange}
-					/>
-				</div>
-				<div className={st.submit}>
-					<Buttons text_="Iniciar sesión" link_="#" click_={handleSubmit} />
-				</div>
-			</div>
-		</>
-	);
+    return (
+        <>
+            <form
+                onSubmit={(e) => {
+                    e.preventDefault();
+                    handleSubmit({ email: e.target.email.value, password: e.target.password.value });
+                }}>
+                <div className={st.container}>
+                    <div className={st.inpCont}>
+                        {/* <Inputs
+                            name_='email'
+                            type_='email'
+                            leyend='Correo electrónico'
+                            handleChange={handleChange}
+                        /> */}
+                        <input name='email' type='email' />
+                    </div>
+                    <div className={st.inpCont}>
+                        {/* <Inputs name_='password' type_='password' leyend='Contraseña' handleChange={handleChange} /> */}
+                        <input name='password' type='password' />
+                    </div>
+                    <div className={st.submit}>
+                        <button type='submit'>Iniciar sesión</button>
+                        {/* <Buttons text_='Iniciar sesión' link_='#' click_={handleSubmit} /> */}
+                    </div>
+                </div>
+            </form>
+        </>
+    );
 }
