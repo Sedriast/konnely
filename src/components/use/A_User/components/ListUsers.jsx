1aacb90221fdba118a33bc2b5e5ffe37b271ff47
exSED <sedriast@gmail.com>
2022-08-20 11:04:53 -0500

    (Incomplete) Logic changes and style fixes in the 'User' component

--- src/components/use/A_User/components/ListUsers.jsx
+++ src/components/use/A_User/components/ListUsers.jsx
@@ -3,7 +3,7 @@ import st from '../style/ListUsers.module.css';
 export function ListUsers() {
 	return (
 		<>
-			<div className={st.l}></div>
+			<div className={st.panelList}>Lista de usuarios (Eliminar-agregar-editar )</div>
 		</>
 	);
 }
