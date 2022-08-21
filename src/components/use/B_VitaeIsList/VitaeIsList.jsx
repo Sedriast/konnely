a418ff5e906dbf792a4ede7d8e7719b4a8100f21
exSED <sedriast@gmail.com>
2022-08-20 23:53:33 -0500

    (Incomplete) Aesthetic changes in 'Vitae´s list'

--- src/components/use/B_VitaeIsList/VitaeIsList.jsx
+++ src/components/use/B_VitaeIsList/VitaeIsList.jsx
@@ -1,10 +1,15 @@
-import st from './styles/List.module.css';
+import st from './styles/VitaeIsList.module.css';
 
 import { List } from './components/List';
 
 export function ViewIsList() {
 	return (
 		<>
+			<div className={st.leftMenu}>
+				<figure title="Nuevo conejo" tooltip-dir="rigth">
+					<button>➕</button>
+				</figure>
+			</div>
 			<List />
 		</>
 	);
