d5465a960b01b7deaf2117fb595f64134d372592
exSED <sedriast@gmail.com>
2022-08-25 23:18:14 -0500

    Style changes in 'User'

--- src/components/use/A_User/components/ListUsers/ListUsers.jsx
+++ src/components/use/A_User/components/ListUsers/ListUsers.jsx
@@ -3,20 +3,26 @@ import { UserCards } from './components/UserCards';
 import { SearchAll } from '../../../../firebase/funtions/SearchAll';
 
 export function ListUsers() {
-    const data = SearchAll('usuarios').props.children;
-    return (
-        <>
-            <div className={st.container}>
-                <div className={st.panelList}>
-                    {data[0].uid !== null && (
-                        <>
-                            {data.map((users, index) => (
-                                <UserCards key={index} userName={users.user} userEmail={users.email} />
-                            ))}
-                        </>
-                    )}
-                </div>
-            </div>
-        </>
-    );
+	const data = SearchAll('usuarios').props.children;
+	return (
+		<>
+			<div className={st.panelSearchBar}>
+				<input placeholder="Buscar" />
+				<button type="submit" className={st.btnSearch} onClick={() => {}}>
+					🔎
+				</button>
+			</div>
+			<div className={st.container}>
+				<div className={st.panelList}>
+					{data[0].uid !== null && (
+						<>
+							{data.map((users, index) => (
+								<UserCards key={index} userName={users.user} userEmail={users.email} />
+							))}
+						</>
+					)}
+				</div>
+			</div>
+		</>
+	);
 }
--- src/components/use/A_User/components/ListUsers/components/UserCards.jsx
+++ src/components/use/A_User/components/ListUsers/components/UserCards.jsx
@@ -1,19 +1,15 @@
 import st from '../styles/UserCards.module.css';
 import { Buttons } from '../../../../0-GeneralComp/F-Buttons/Buttons';
 export function UserCards({ userEmail, userId, userName }) {
-    return (
-        <div className={st.container}>
-            <div>
-                <div>{userName}</div>
-                <br />
-                <div>{userId}</div>
-            </div>
+	return (
+		<div className={st.container}>
+			<h1>{userName}</h1>
 
-            <div className={st.mid}>{userEmail}</div>
+			<div className={st.mid}>{userEmail}</div>
 
-            <div className={st.btnEdit}>
-                <Buttons route='/user' btnIconText='✏️' label='Editar' />
-            </div>
-        </div>
-    );
+			<div className={st.btnEdit}>
+				<Buttons route="/user" btnIconText="✏️" label="Editar" direction="left" />
+			</div>
+		</div>
+	);
 }
