13d3749667498413c8a6f95bfdab97bb1b66362a
JfAlonso <alonsofelipe8888@gmail.com>
2022-08-28 14:56:56 -0500

    FormEdit:Update

--- src/components/use/0-GeneralComp/F-List/Lists.jsx
+++ src/components/use/0-GeneralComp/F-List/Lists.jsx
@@ -1,22 +1,22 @@
 import st from './styles/Lists.module.css';
 
 export function Lists(props) {
-	const action = (e) => {
-		props.handleChange(e);
-	};
+    const action = (e) => {
+        props.handleChange(e);
+    };
 
-	return (
-		<>
-			<div className={st.container}>
-				<h1>{props.leyend}</h1>
-				<select className={st.list_} name={props.name_} onChange={action}>
-					{props.listar?.map((a) => (
-						<option key={a} value={a}>
-							{a}
-						</option>
-					))}
-				</select>
-			</div>
-		</>
-	);
+    return (
+        <>
+            <div className={st.container}>
+                <h1>{props.leyend}</h1>
+                <select className={st.list_} name={props.name_} onChange={action} defaultValue={props.value_}>
+                    {props.listar?.map((a) => (
+                        <option key={a} value={a}>
+                            {a}
+                        </option>
+                    ))}
+                </select>
+            </div>
+        </>
+    );
 }
--- src/components/use/C_DataView/scripts/dataProv.js
+++ src/components/use/C_DataView/scripts/dataProv.js
@@ -1,107 +1,107 @@
-export const recuperar = (id, url) => {
-	basicData.id = id;
-	basicData.url = url;
+export const recuperar = (id, info) => {
+    basicData.id = id;
+    basicData.info = info;
 };
 export const basicData = {
-	id: null,
-	url: null,
+    id: null,
+    info: {},
 };
 export const ciclo = [
-	{
-		etapa: 'Nacimiento',
-		peso: '200gr',
-		fecha: '**-**-****',
-	},
-	{
-		etapa: 'Lactancia',
-		peso: '1kg',
-		fecha: '**-**-****',
-	},
-	{
-		etapa: 'Levante',
-		peso: '1.5kg',
-		fecha: '**-**-****',
-	},
-	{
-		etapa: 'Engorde',
-		peso: '2kg',
-		fecha: '**-**-****',
-	},
-	{
-		etapa: 'Ceba',
-		peso: '2.5kg',
-		fecha: '**-**-****',
-	},
+    {
+        etapa: 'Nacimiento',
+        peso: '200gr',
+        fecha: '**-**-****',
+    },
+    {
+        etapa: 'Lactancia',
+        peso: '1kg',
+        fecha: '**-**-****',
+    },
+    {
+        etapa: 'Levante',
+        peso: '1.5kg',
+        fecha: '**-**-****',
+    },
+    {
+        etapa: 'Engorde',
+        peso: '2kg',
+        fecha: '**-**-****',
+    },
+    {
+        etapa: 'Ceba',
+        peso: '2.5kg',
+        fecha: '**-**-****',
+    },
 ];
 export const reproData = {
-	rechazos: '0',
-	muertes: '0',
-	vivos: '0',
-	partos: '0',
+    rechazos: '0',
+    muertes: '0',
+    vivos: '0',
+    partos: '0',
 };
 export const tratamientos = [
-	{
-		idTratamiento: '1',
-		fecha: '**-**-****',
-		title: 'Cancer',
-	},
-	{
-		idTratamiento: '2',
-		fecha: '**-**-****',
-		title: 'Cancer',
-	},
-	{
-		idTratamiento: '3',
-		fecha: '**-**-****',
-		title: 'cancer',
-	},
-	{
-		idTratamiento: '4',
-		fecha: '**-**-****',
-		title: 'cancer',
-	},
-	{
-		idTratamiento: '5',
-		fecha: '**-**-****',
-		title: 'cancer',
-	},
-	{
-		idTratamiento: '6',
-		fecha: '**-**-****',
-		title: 'cancer',
-	},
-	{
-		idTratamiento: '7',
-		fecha: '**-**-****',
-		title: 'cancer',
-	},
-	{
-		idTratamiento: '8',
-		fecha: '**-**-****',
-		title: 'cancer',
-	},
-	{
-		idTratamiento: '9',
-		fecha: '**-**-****',
-		title: 'cancer',
-	},
+    {
+        idTratamiento: '1',
+        fecha: '**-**-****',
+        title: 'Cancer',
+    },
+    {
+        idTratamiento: '2',
+        fecha: '**-**-****',
+        title: 'Cancer',
+    },
+    {
+        idTratamiento: '3',
+        fecha: '**-**-****',
+        title: 'cancer',
+    },
+    {
+        idTratamiento: '4',
+        fecha: '**-**-****',
+        title: 'cancer',
+    },
+    {
+        idTratamiento: '5',
+        fecha: '**-**-****',
+        title: 'cancer',
+    },
+    {
+        idTratamiento: '6',
+        fecha: '**-**-****',
+        title: 'cancer',
+    },
+    {
+        idTratamiento: '7',
+        fecha: '**-**-****',
+        title: 'cancer',
+    },
+    {
+        idTratamiento: '8',
+        fecha: '**-**-****',
+        title: 'cancer',
+    },
+    {
+        idTratamiento: '9',
+        fecha: '**-**-****',
+        title: 'cancer',
+    },
 ];
 
 export const datos = [
-	{
-		name: 'Loading...',
-		url: null,
-		genero: 'Loading...',
-		raza: 'Loading...',
-		concepcion: 'Loading...',
-		grupo: 'Loading...',
-		destete: 'Loading...',
-		uid: 'Loading...',
-		idMadre: 'Loading...',
-		idPadre: 'Loading...',
-		motivo: 'Loading...',
-		nacimiento: 'Loading...',
-		peso: 'Loading...',
-		tema: 'Loading...',
-	},
+    {
+        name: 'Loading...',
+        url: null,
+        genero: 'Loading...',
+        raza: 'Loading...',
+        concepcion: 'Loading...',
+        grupo: 'Loading...',
+        destete: 'Loading...',
+        id: 'Loading...',
+        idMadre: 'Loading...',
+        idPadre: 'Loading...',
+        motivo: 'Loading...',
+        nacimiento: 'Loading...',
+        peso: 'Loading...',
+        tema: 'Loading...',
+    },
 ];
