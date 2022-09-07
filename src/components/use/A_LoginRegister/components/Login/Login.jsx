6ff8f938d67b404d5a93f0fddbc853e025076e88
JfAlonso <alonsofelipe8888@gmail.com>
2022-09-07 15:32:31 -0500

    update:login

--- src/components/use/A_LoginRegister/components/Login/Login.jsx
+++ src/components/use/A_LoginRegister/components/Login/Login.jsx
@@ -1,4 +1,3 @@
-import swal from 'sweetalert';
 import st from './Login.module.css';
 
 import { useAuth } from '../../../../../context/AuthContext';
@@ -6,48 +5,39 @@ import { ValidationErrors } from '../../scripts/ValidationErrors';
 import { useNavigate } from 'react-router-dom';
 
 export function Login() {
-	const navigate = useNavigate();
-	const { login, user } = useAuth();
-	console.log(user);
+    const navigate = useNavigate();
+    const { login } = useAuth();
 
-	const handleSubmit = async (e) => {
-		try {
-			await login(e.email, e.password);
-		} catch (error) {
-			ValidationErrors(error.code);
-		}
-	};
-	if (user !== null && user.emailVerified === true) {
-		navigate('/vitaeslist');
-	} else {
-		swal({
-			title: 'Verifique su cuenta en la bandeja de su correo electronico',
-			icon: 'error',
-			button: 'aceptar',
-		});
-	}
-	return (
-		<>
-			<div className={st.container}>
-				<form
-					onSubmit={(e) => {
-						e.preventDefault();
-						handleSubmit({ email: e.target.email.value, password: e.target.password.value });
-					}}
-				>
-					<div className={st.inpCont}>
-						<h3>Usuario institucional</h3>
-						<input name="email" type="email" />
-					</div>
-					<div className={st.inpCont}>
-						<h3>Contraseña</h3>
-						<input name="password" type="password" />
-					</div>
-					<div className={st.submit}>
-						<button type="submit">Iniciar sesión</button>
-					</div>
-				</form>
-			</div>
-		</>
-	);
+    const handleSubmit = async (e) => {
+        try {
+            await login(e.email + '@ucundinamarca.edu.co', e.password).then(() => {
+                navigate('/vitaeslist');
+            });
+        } catch (error) {
+            ValidationErrors(error.code);
+        }
+    };
+    return (
+        <>
+            <div className={st.container}>
+                <form
+                    onSubmit={(e) => {
+                        e.preventDefault();
+                        handleSubmit({ email: e.target.email.value, password: e.target.password.value });
+                    }}>
+                    <div className={st.inpCont}>
+                        <h3>Usuario institucional</h3>
+                        <input name='email' type='text' autocomplete='username' />
+                    </div>
+                    <div className={st.inpCont}>
+                        <h3>Contraseña</h3>
+                        <input name='password' type='password' />
+                    </div>
+                    <div className={st.submit}>
+                        <button type='submit'>Iniciar sesión</button>
+                    </div>
+                </form>
+            </div>
+        </>
+    );
 }
