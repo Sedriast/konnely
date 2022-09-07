e69c536152eb1c1b32b2bf584e1c819f3f8207e7
JfAlonso <alonsofelipe8888@gmail.com>
2022-09-07 16:15:23 -0500

    Update:list

--- src/components/use/B_VitaeIsList/components/List/List.jsx
+++ src/components/use/B_VitaeIsList/components/List/List.jsx
@@ -6,90 +6,90 @@ import { useState } from 'react';
 import { DropdownList } from './components/DropdownList';
 
 export function List() {
-	let search = { vieja: 'init' };
-	const [filter, setFilter] = useState('Hembra');
-	const [search_, setSearch_] = useState('init');
-	const [parametro, setParametro] = useState('');
-	const [auxsearch_, setAuxsearch_] = useState('');
-	function reset() {
-		setAuxsearch_('');
-		setParametro('');
-		setFilter('Hembra');
-	}
-	return (
-		<>
-			<div className={st.container}>
-				<div className={st.panelSearchBar}>
-					<input
-						value={parametro}
-						placeholder="Buscar"
-						onChange={(e) => {
-							setAuxsearch_(e.target.value);
-							setParametro(e.target.value);
-						}}
-					/>
-					<button
-						className={st.btnSearch}
-						onClick={() => {
-							if (auxsearch_ === '') {
-								swal({
-									title: 'Digite un identificador válido.',
-									icon: 'error',
-									button: 'aceptar',
-								});
-							} else {
-								setSearch_(auxsearch_);
-								setFilter('');
-								setParametro('');
-								setAuxsearch_('init');
-							}
-						}}
-					>
-						🔎
-					</button>
-					<div className={st.filter}>
-						<figure title="Filtrar machos" tooltip-dir="bottom">
-							<button
-								id="b"
-								className={st.male}
-								onClick={() => {
-									setSearch_('');
-									setAuxsearch_('Macho');
-									setFilter('Macho');
-								}}
-							>
-								♂️
-							</button>
-						</figure>
-						<figure title="Filtrar hembras" tooltip-dir="bottom">
-							<button
-								id="r"
-								className={st.female}
-								onClick={() => {
-									setSearch_('');
-									setAuxsearch_('Hembra');
-									setFilter('Hembra');
-								}}
-							>
-								♀️
-							</button>
-						</figure>
-					</div>
-				</div>
-				<div className={st.panelCards}>
-					<div className={st.cards}>
-						{filter === 'Hembra' && (
-							<DropdownList coleccion="conejos" parametro="genero" busqueda={filter} />
-						)}
-						{filter === 'Macho' && (
-							<DropdownList coleccion="conejos" parametro="genero" busqueda={filter} />
-						)}
-						{auxsearch_ === search.vieja && (
-							<DropdownList coleccion="conejos" parametro="id" busqueda={search_} reset={reset} />
-						)}
-					</div>
-				</div>
-			</div>
-		</>
-	);
+    let search = { vieja: 'init' };
+    const [filter, setFilter] = useState('Hembra');
+    const [search_, setSearch_] = useState('init');
+    const [parametro, setParametro] = useState('');
+    const [auxsearch_, setAuxsearch_] = useState('');
+    function reset() {
+        setAuxsearch_('');
+        setParametro('');
+        setFilter('Hembra');
+    }
+    return (
+        <>
+            <div className={st.container}>
+                <div className={st.panelSearchBar}>
+                    <form
+                        onSubmit={(e) => {
+                            e.preventDefault();
+                            if (auxsearch_ === '') {
+                                swal({
+                                    title: 'Digite un identificador válido.',
+                                    icon: 'error',
+                                    button: 'aceptar',
+                                });
+                            } else {
+                                setSearch_(auxsearch_);
+                                setFilter('');
+                                setParametro('');
+                                setAuxsearch_('init');
+                            }
+                        }}>
+                        <input
+                            name='buscar'
+                            value={parametro}
+                            placeholder='Buscar'
+                            onChange={(e) => {
+                                setAuxsearch_(e.target.value);
+                                setParametro(e.target.value);
+                            }}
+                        />
+                        <button className={st.btnSearch} type='submit'>
+                            🔎
+                        </button>
+                    </form>
+                    <div className={st.filter}>
+                        <figure title='Filtrar machos' tooltip-dir='bottom'>
+                            <button
+                                id='b'
+                                className={st.male}
+                                onClick={() => {
+                                    setSearch_('');
+                                    setAuxsearch_('Macho');
+                                    setFilter('Macho');
+                                }}>
+                                ♂️
+                            </button>
+                        </figure>
+                        <figure title='Filtrar hembras' tooltip-dir='bottom'>
+                            <button
+                                id='r'
+                                className={st.female}
+                                onClick={() => {
+                                    setSearch_('');
+                                    setAuxsearch_('Hembra');
+                                    setFilter('Hembra');
+                                }}>
+                                ♀️
+                            </button>
+                        </figure>
+                    </div>
+                </div>
+                <div className={st.panelCards}>
+                    <div className={st.cards}>
+                        {filter === 'Hembra' && (
+                            <DropdownList coleccion='conejos' parametro='genero' busqueda={filter} />
+                        )}
+                        {filter === 'Macho' && (
+                            <DropdownList coleccion='conejos' parametro='genero' busqueda={filter} />
+                        )}
+                        {auxsearch_ === search.vieja && (
+                            <DropdownList coleccion='conejos' parametro='id' busqueda={search_} reset={reset} />
+                        )}
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 }
