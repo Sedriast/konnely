39dcaa37e957236677b028a57d7f2b7e64e4698f
exSED <sedriast@gmail.com>
2022-09-11 20:49:06 -0500

    AddFakeData

---
+++ src/components/use/0-GeneralComp/0-fakeData/reproductiveCycle.js
@@ -0,0 +1,55 @@
+export const litter = [
+	{
+		id: 'Nacimiento',
+		editors: [
+			{
+				idUser: 123456,
+				userName: 'Pepito',
+			},
+			{
+				idUser: 123,
+				userName: 'Juanito',
+			},
+		],
+		state: '10/20/2030',
+		appidMother: 123456,
+		stages: [
+            {
+            stage: 'Monta/Servicio',
+            state:  Hecho,
+            date: 20-12-2022,
+            males: [1234, 1223],
+            },
+           {
+            stage: 'Palpación',
+            state:  'Hecho',
+            approximateDate: 20-12-2022,
+            realDate: 21-12-2022,
+            },
+            {
+            stage: 'PreParto',
+            state:  'Hecho',
+            approximateDate: 20-12-2022,
+            realDate: 21-12-2022
+            }
+            {
+            stage: 'Parto',
+            state:  'Hecho',
+            approximateDate: '20-12-2022',
+            lives: 20,
+            deaths: 2,
+            camadaWeigth: '8000gr',
+            homogeneizados: 2,
+            total: 24,
+            realDate: '21-12-2022',
+            },
+            {
+            stage: 'Destete',
+            state:  'Hecho',
+            approximateDate: '20-12-2022',
+            total: 24,
+            averageWeight: '1000gr',
+            realDate: 21-12-2022,
+            },],
+	},
+];
--- src/components/use/C_DataView/components/Vitae/components/ReproductiveCycle/ReproductiveCycle.jsx
+++ src/components/use/C_DataView/components/Vitae/components/ReproductiveCycle/ReproductiveCycle.jsx
@@ -1,11 +1,14 @@
 import st from './ReproductiveCycle.module.css';
 
+import { litter } from '../../../../../0-GeneralComp/0-fakeData/reproductiveCycle';
+
 export function ReproductiveCycle() {
-	
 	return (
 		<>
 			<div className={st.container}>
-
+				{litter[0].stages.map(() => {
+					<div></div>;
+				})}
 			</div>
 		</>
 	);
