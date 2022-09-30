e39c4911244552113272de7e03aa93698ceb7863
JfAlonso <alonsofelipe8888@gmail.com>
2022-09-29 19:53:51 -0500

    basicData:Erros

--- src/components/use/C_DataView/components/Vitae/components/RabbitDataView/RabbitDataView.jsx
+++ src/components/use/C_DataView/components/Vitae/components/RabbitDataView/RabbitDataView.jsx
@@ -3,7 +3,7 @@ import st from './RabbitDataView.module.css';
 import app from '../../../../../../firebase/credentials';
 
 import { useEffect, useState } from 'react';
-import { recuperar } from '../../../../../0-GeneralComp/0-StaticData/dataProv';
+import { basicData } from '../../../../../0-GeneralComp/0-StaticData/dataProv';
 import { collection, getFirestore, onSnapshot, query, where } from 'firebase/firestore';
 import { Buttons } from '../../../../../0-GeneralComp/1-Buttons/Buttons';
 import { faPenToSquare } from '@fortawesome/free-solid-svg-icons';
@@ -11,88 +11,88 @@ import { faPenToSquare } from '@fortawesome/free-solid-svg-icons';
 const db = getFirestore(app);
 
 export function RabbitDataView({ stageId }) {
-	const [data_, setData_] = useState([]);
-	useEffect(() => {
-		const q = query(collection(db, 'rabbits'), where('id', '==', stageId));
-		onSnapshot(q, (snapshot) => setData_(snapshot.docs.map((doc) => ({ ...doc.data() }))));
-	}, [stageId]);
+    const [data_, setData_] = useState([]);
+    useEffect(() => {
+        const q = query(collection(db, 'rabbits'), where('id', '==', stageId));
+        onSnapshot(q, (snapshot) => setData_(snapshot.docs.map((doc) => ({ ...doc.data() }))));
+    }, [stageId]);
 
-	return (
-		<>
-			{recuperar.id !== null && (
-				<>
-					<div className={st.initInfo}>
-						<div className={st.rabbitImg}>
-							<img src={data_[0]?.url} alt="" />
-						</div>
-						<br />
-						{data_[0]?.id}
-						<br />
-						<br />
-						<hr />
-						<br />
-					</div>
-					<div className={st.tit}>
-						Datos Básicos
-						<div>
-							<Buttons
-								route="/formEdit"
-								label="Editar"
-								direction="bottom"
-								btnIconText={faPenToSquare}
-							/>
-						</div>
-					</div>
-					<br />
-					<br />
-					<br />
-					<div className={st.panelData}>
-						<div className={st.titles}>
-							Estado:
-							<br />
-							<br />
-							Raza:
-							<br />
-							Genero:
-							<br />
-							<br />
-							Concepción:
-							<br />
-							Fecha:
-							<br />
-							Procedencia:
-							<br />
-							<br />
-							ID. Madre:
-							<br />
-							ID. Padre:
-						</div>
-						<div className={st.ask}>
-							----------------------------
-							<br />
-							<br />
-							{data_[0]?.raza} ({data_[0]?.porcentaje}%)
-							<br />
-							{data_[0]?.genero}
-							<br />
-							<br />
-							{data_[0]?.concepcion}
-							<br />
-							{data_[0]?.nacimiento}
-							<br />
-							{data_[0]?.origen}
-							<br />
-							<br />
-							{data_[0]?.idMadre}
-							<br />
-							{data_[0]?.idPadre}
-						</div>
-					</div>
-					<br />
-					<br />
-					<br />
-				</>
-			)}
-		</>
-	);
+    return (
+        <>
+            {basicData.id !== null && (
+                <>
+                    <div className={st.initInfo}>
+                        <div className={st.rabbitImg}>
+                            <img src={data_[0]?.url} alt='' />
+                        </div>
+                        <br />
+                        {data_[0]?.id}
+                        <br />
+                        <br />
+                        <hr />
+                        <br />
+                    </div>
+                    <div className={st.tit}>
+                        Datos Básicos
+                        <div>
+                            <Buttons
+                                route='/formEdit'
+                                label='Editar'
+                                direction='bottom'
+                                btnIconText={faPenToSquare}
+                            />
+                        </div>
+                    </div>
+                    <br />
+                    <br />
+                    <br />
+                    <div className={st.panelData}>
+                        <div className={st.titles}>
+                            Estado:
+                            <br />
+                            <br />
+                            Raza:
+                            <br />
+                            Genero:
+                            <br />
+                            <br />
+                            Concepción:
+                            <br />
+                            Fecha:
+                            <br />
+                            Procedencia:
+                            <br />
+                            <br />
+                            ID. Madre:
+                            <br />
+                            ID. Padre:
+                        </div>
+                        <div className={st.ask}>
+                            ----------------------------
+                            <br />
+                            <br />
+                            {data_[0]?.raza} ({data_[0]?.porcentaje}%)
+                            <br />
+                            {data_[0]?.genero}
+                            <br />
+                            <br />
+                            {data_[0]?.concepcion}
+                            <br />
+                            {data_[0]?.nacimiento}
+                            <br />
+                            {data_[0]?.origen}
+                            <br />
+                            <br />
+                            {data_[0]?.idMadre}
+                            <br />
+                            {data_[0]?.idPadre}
+                        </div>
+                    </div>
+                    <br />
+                    <br />
+                    <br />
+                </>
+            )}
+        </>
+    );
 }
--- src/components/use/F-Forms/EditRabbitData/components/Form/Form.jsx
+++ src/components/use/F-Forms/EditRabbitData/components/Form/Form.jsx
@@ -8,7 +8,7 @@ import { faCamera } from '@fortawesome/free-solid-svg-icons';
 
 import { useModal } from '../../../../0-GeneralComp/0-StaticData/Modals/useModal';
 import { EditImageAndInfo } from '../../../../../firebase/funtions/AddInformation';
-import { recuperar } from '../../../../0-GeneralComp/0-StaticData/dataProv';
+import { recuperar, basicData } from '../../../../0-GeneralComp/0-StaticData/dataProv';
 
 import { DropdownForm } from './components/DropdownForm';
 import { Modal } from '../../../../0-GeneralComp/0-StaticData/Modals/Modal';
@@ -19,204 +19,201 @@ import { conditionalBasis } from '../../../../0-GeneralComp/0-StaticData/Dates/c
 import { GetDocument } from '../../../../../firebase/funtions/GetDocument';
 
 export function Form() {
-	const navigate = useNavigate();
-	const genero = ['Género', 'Hembra', 'Macho'];
-	const concepcion = ['Concepción', 'Monta natural', 'Inseminación artificial'];
-	const [reason, setReason] = useState();
-	const [image, setImage] = useState(null);
-	const [values, setValues] = useState({});
-	const [image_, setImage_] = useState(null);
-	const [auxImage_, setAuxImage_] = useState(null);
-	const [isOpenModal, openModal, closeModal] = useModal(false);
+    const navigate = useNavigate();
+    const genero = ['Género', 'Hembra', 'Macho'];
+    const concepcion = ['Concepción', 'Monta natural', 'Inseminación artificial'];
+    const [reason, setReason] = useState();
+    const [image, setImage] = useState(null);
+    const [values, setValues] = useState({});
+    const [image_, setImage_] = useState(null);
+    const [auxImage_, setAuxImage_] = useState(null);
+    const [isOpenModal, openModal, closeModal] = useModal(false);
 
-	const updateState = (name, value) => {
-		if (name === 'nacimiento') {
-			setValues({ ...values, [name]: value });
-		} else if (name === 'motivo') {
-			setReason(value);
-			setValues({ ...values, [name]: value });
-		} else {
-			setValues({ ...values, [name]: value });
-		}
-	};
+    const updateState = (name, value) => {
+        if (name === 'nacimiento') {
+            setValues({ ...values, [name]: value });
+        } else if (name === 'motivo') {
+            setReason(value);
+            setValues({ ...values, [name]: value });
+        } else {
+            setValues({ ...values, [name]: value });
+        }
+    };
 
-	function handleChange(e) {
-		const { name, value } = e.target;
-		if (name === 'motivo') {
-			updateState(name, value);
-		} else if (name === 'nacimiento') {
-			e.target.value = conditionalBasis(updateState, name, value);
-		} else if (name === 'traslado') {
-			e.target.value = conditionalBasis(updateState, name, value);
-		} else {
-			updateState(name, value);
-		}
-	}
-	const handleSubmit = (aux) => {
-		EditImageAndInfo({
-			...aux,
-			uid: recuperar.info.uid,
-			image: image,
-		});
-		recuperar(values.id);
-	};
-	useEffect(() => {
-		if (recuperar.id !== null) {
-			setImage_(recuperar.info.url);
-			setReason(recuperar.info.motivo);
-		} else {
-			navigate('/vitaeslist');
-			return null;
-		}
-	}, [navigate]);
-	return (
-		<>
-			{recuperar.id !== null && (
-				<div className={st.container}>
-					<form
-						onSubmit={(e) => {
-							e.preventDefault();
-							let aux = {};
-							for (const element of e.target) {
-								if (element.name !== '') {
-									aux = { ...aux, [element.name]: element.value };
-								}
-							}
-							handleSubmit(aux);
-						}}
-					>
-						<div className={st.panelContainer}>
-							<div className={st.panelInpImg}>
-								<div className={st.panelImage}>
-									<Inputs
-										type="file"
-										HaveImage={(e) => {
-											setImage(e);
-											if (e === null) {
-												setImage_(null);
-												setAuxImage_(null);
-											}
-										}}
-										preeview={image_}
-									/>
-									<Modal isOpen={isOpenModal} closeModal={closeModal}>
-										{isOpenModal && (
-											<>
-												<Webcam
-													audio={false}
-													height={250}
-													width={330}
-													screenshotFormat="image/jpeg"
-												>
-													{({ getScreenshot }) => (
-														<button
-															onClick={() => {
-																const imageSrc = getScreenshot();
-																setAuxImage_(imageSrc);
-															}}
-														>
-															Capturar foto
-														</button>
-													)}
-												</Webcam>
-												<img src={auxImage_} alt=""></img>
-												<button
-													onClick={() => {
-														setImage_(auxImage_);
-														closeModal();
-													}}
-												>
-													Aceptar
-												</button>
-											</>
-										)}
-									</Modal>
-								</div>
-								<div className={st.btnC}>
-									<Buttons
-										direction="bottom"
-										label="Cámara"
-										btnIconText={faCamera}
-										btnClick={openModal}
-										route="#"
-									/>
-								</div>
-							</div>
-							<div className={st.panel}>
-								<Inputs
-									value={recuperar?.id}
-									leyend="Identificador"
-									name="id"
-									placeholder="Ingrese el identificador"
-									type="number"
-									inputmode="numeric"
-									handleChange={handleChange}
-								/>
-								<Lists
-									value_={recuperar?.info?.genero}
-									leyend="Género"
-									name_="genero"
-									listar={genero}
-									handleChange={handleChange}
-								/>
-								<Lists
-									value_={recuperar?.info?.raza}
-									leyend="Raza"
-									name_="raza"
-									listar={
-										GetDocument({ coleccion: 'lists', list: 'races' }).props.children[0].values
-									}
-									handleChange={handleChange}
-								/>
-								<Inputs
-									value={recuperar?.info?.porcentaje}
-									leyend="Porcentaje de pureza"
-									name="porcentaje"
-									placeholder="Procentaje de pureza"
-									type="number"
-									inputmode="numeric"
-									handleChange={handleChange}
-								/>
-								<Lists
-									value_={recuperar?.info?.concepcion}
-									leyend="Concepción"
-									name_="concepcion"
-									listar={concepcion}
-									handleChange={handleChange}
-								/>
-								<Inputs
-									value={recuperar?.info?.nacimiento}
-									leyend="Fecha de nacimiento"
-									name="nacimiento"
-									type="date"
-									handleChange={handleChange}
-								/>
-								{/* {date && <DropdownDate date={date} handleChange={handleChange} />} */}
-								<Lists
-									value_={recuperar?.info?.motivo}
-									leyend="Motivo de ingreso"
-									name_="motivo"
-									listar={
-										GetDocument({ coleccion: 'lists', list: 'reasons' }).props.children[0].values
-									}
-									handleChange={handleChange}
-								/>
-								{reason && <DropdownForm motivo={reason} handleChange={handleChange} />}
-							</div>
-							<div className={st.submit}>
-								{/* <Buttons
+    function handleChange(e) {
+        const { name, value } = e.target;
+        if (name === 'motivo') {
+            updateState(name, value);
+        } else if (name === 'nacimiento') {
+            e.target.value = conditionalBasis(updateState, name, value);
+        } else if (name === 'traslado') {
+            e.target.value = conditionalBasis(updateState, name, value);
+        } else {
+            updateState(name, value);
+        }
+    }
+    const handleSubmit = (aux) => {
+        EditImageAndInfo({
+            ...aux,
+            uid: basicData.info.uid,
+            image: image,
+        });
+        recuperar(values.id);
+    };
+    useEffect(() => {
+        if (basicData.id !== null) {
+            setImage_(basicData.info.url);
+            setReason(basicData.info.motivo);
+        } else {
+            navigate('/vitaeslist');
+            return null;
+        }
+    }, [navigate]);
+    return (
+        <>
+            {basicData.id !== null && (
+                <div className={st.container}>
+                    <form
+                        onSubmit={(e) => {
+                            e.preventDefault();
+                            let aux = {};
+                            for (const element of e.target) {
+                                if (element.name !== '') {
+                                    aux = { ...aux, [element.name]: element.value };
+                                }
+                            }
+                            handleSubmit(aux);
+                        }}>
+                        <div className={st.panelContainer}>
+                            <div className={st.panelInpImg}>
+                                <div className={st.panelImage}>
+                                    <Inputs
+                                        type='file'
+                                        HaveImage={(e) => {
+                                            setImage(e);
+                                            if (e === null) {
+                                                setImage_(null);
+                                                setAuxImage_(null);
+                                            }
+                                        }}
+                                        preeview={image_}
+                                    />
+                                    <Modal isOpen={isOpenModal} closeModal={closeModal}>
+                                        {isOpenModal && (
+                                            <>
+                                                <Webcam
+                                                    audio={false}
+                                                    height={250}
+                                                    width={330}
+                                                    screenshotFormat='image/jpeg'>
+                                                    {({ getScreenshot }) => (
+                                                        <button
+                                                            onClick={() => {
+                                                                const imageSrc = getScreenshot();
+                                                                setAuxImage_(imageSrc);
+                                                            }}>
+                                                            Capturar foto
+                                                        </button>
+                                                    )}
+                                                </Webcam>
+                                                <img src={auxImage_} alt=''></img>
+                                                <button
+                                                    onClick={() => {
+                                                        setImage_(auxImage_);
+                                                        closeModal();
+                                                    }}>
+                                                    Aceptar
+                                                </button>
+                                            </>
+                                        )}
+                                    </Modal>
+                                </div>
+                                <div className={st.btnC}>
+                                    <Buttons
+                                        direction='bottom'
+                                        label='Cámara'
+                                        btnIconText={faCamera}
+                                        btnClick={openModal}
+                                        route='#'
+                                    />
+                                </div>
+                            </div>
+                            <div className={st.panel}>
+                                <Inputs
+                                    value={basicData?.id}
+                                    leyend='Identificador'
+                                    name='id'
+                                    placeholder='Ingrese el identificador'
+                                    type='number'
+                                    inputmode='numeric'
+                                    handleChange={handleChange}
+                                />
+                                <Lists
+                                    value_={basicData?.info?.genero}
+                                    leyend='Género'
+                                    name_='genero'
+                                    listar={genero}
+                                    handleChange={handleChange}
+                                />
+                                <Lists
+                                    value_={basicData?.info?.raza}
+                                    leyend='Raza'
+                                    name_='raza'
+                                    listar={
+                                        GetDocument({ coleccion: 'lists', list: 'races' }).props.children[0].values
+                                    }
+                                    handleChange={handleChange}
+                                />
+                                <Inputs
+                                    value={basicData?.info?.porcentaje}
+                                    leyend='Porcentaje de pureza'
+                                    name='porcentaje'
+                                    placeholder='Procentaje de pureza'
+                                    type='number'
+                                    inputmode='numeric'
+                                    handleChange={handleChange}
+                                />
+                                <Lists
+                                    value_={basicData?.info?.concepcion}
+                                    leyend='Concepción'
+                                    name_='concepcion'
+                                    listar={concepcion}
+                                    handleChange={handleChange}
+                                />
+                                <Inputs
+                                    value={basicData?.info?.nacimiento}
+                                    leyend='Fecha de nacimiento'
+                                    name='nacimiento'
+                                    type='date'
+                                    handleChange={handleChange}
+                                />
+                                {/* {date && <DropdownDate date={date} handleChange={handleChange} />} */}
+                                <Lists
+                                    value_={basicData?.info?.motivo}
+                                    leyend='Motivo de ingreso'
+                                    name_='motivo'
+                                    listar={
+                                        GetDocument({ coleccion: 'lists', list: 'reasons' }).props.children[0]
+                                            .values
+                                    }
+                                    handleChange={handleChange}
+                                />
+                                {reason && <DropdownForm motivo={reason} handleChange={handleChange} />}
+                            </div>
+                            <div className={st.submit}>
+                                {/* <Buttons
                                     direction='bottom'
                                     label='Guardar'
                                     btnClick={handleSubmit}
                                     route='/vitaeslist'
 									btnIconText={faPaperPlane}
 								/> */}
-								<button type="Submit">Hola</button>
-							</div>
-						</div>
-					</form>
-				</div>
-			)}
-		</>
-	);
+                                <button type='Submit'>Hola</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            )}
+        </>
+    );
 }
