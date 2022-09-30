4a2c3687b1c6c9df4779b5655749a62a52e8d8b5
JfAlonso <alonsofelipe8888@gmail.com>
2022-09-29 19:10:28 -0500

    Update:Healt

--- src/components/use/C_DataView/components/Healt/Healt.jsx
+++ src/components/use/C_DataView/components/Healt/Healt.jsx
@@ -1,6 +1,6 @@
 import st from './styles/Healt.module.css';
 
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import { faFileCirclePlus } from '@fortawesome/free-solid-svg-icons';
@@ -11,35 +11,40 @@ import { basicData } from '../../../C_DataView/scripts/dataProv';
 import { QueriesSimple_ } from '../../../../firebase/funtions/QueriesSimple_';
 
 export function Healt() {
-	const navigate = useNavigate();
-	const trataments = QueriesSimple_({
-		coleccion: 'trataments',
-		parametro: 'uidRabbit',
-		busqueda: basicData.info.uid,
-	}).props.children;
-	useEffect(() => {
-		if (basicData.id === null) {
-			navigate('/vitaeslist');
-			return null;
-		}
-	}, [navigate]);
-	return (
-		<div className={st.container}>
-			<div className={st.panelSearchBar}>
-				<input placeholder="Buscar" />
-				<button className={st.btnSearch}>🔎</button>
-			</div>
-			<div className={st.panelItems}>
-				<List trataments={trataments} />
-			</div>
-			<div className={st.new}>
-				<Buttons
-					btnIconText={faFileCirclePlus}
-					route="/addTrat"
-					label="Nuevo tratamiento"
-					direction="top"
-				/>
-			</div>
-		</div>
-	);
+    const { search, setSearch } = useState('');
+    const navigate = useNavigate();
+    const trataments = QueriesSimple_({
+        coleccion: 'trataments',
+        parametro: 'uidRabbit',
+        busqueda: basicData.info.uid,
+    }).props.children;
+    useEffect(() => {
+        if (basicData.id === null) {
+            navigate('/vitaeslist');
+            return null;
+        }
+    }, [navigate]);
+    return (
+        <div className={st.container}>
+            <div className={st.panelSearchBar}>
+                <input
+                    placeholder='Buscar'
+                    onChange={(e) => {
+                        console.log(e.target.value);
+                    }}
+                />
+                <button className={st.btnSearch}>🔎</button>
+            </div>
+
+            <div className={st.panelItems}>{search !== '' && <List trataments={trataments} />}</div>
+            <div className={st.new}>
+                <Buttons
+                    btnIconText={faFileCirclePlus}
+                    route='/addTrat'
+                    label='Nuevo tratamiento'
+                    direction='top'
+                />
+            </div>
+        </div>
+    );
 }
