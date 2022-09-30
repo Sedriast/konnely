02c112e6c7da7932d3f0e7deb76e6d32b2b0e8aa
exSED <sedriast@gmail.com>
2022-09-29 20:43:12 -0500

    LifeCycle

--- src/components/use/F-Forms/EditLifedata/Form/Form.jsx
+++ src/components/use/F-Forms/EditLifedata/Form/Form.jsx
@@ -3,70 +3,69 @@ import st from './Form.module.css';
 export function Form({ info }) {
 	return (
 		<>
-			{info?.lifecycle.map((items, index) => {
-				return (
-					<form
-						onSubmit={(e) => {
-							e.preventDefault();
-							for (const element of e.target) {
-								if (element.name !== '') {
-									console.log(element.name + ': ' + element.value);
-								}
-							}
-						}}
-						key={index}
-						className={items.stage === 'Nacimiento' ? st.bird : st.container}
-						action=""
-					>
-						{items.stage === 'Nacimiento' ? (
-							<></>
-						) : (
-							<>
-								<div className={st.idName}>{items.stage}</div>
-								<hr />
-								<br />
-								<br />
-								<br />
-								<div className={st.titles}>
-									Fecha pronosticada:
+			<form
+				className={st.container}
+				onSubmit={(e) => {
+					e.preventDefault();
+					for (const element of e.target) {
+						if (element.name !== '') {
+							console.log(element.name + ': ' + element.value);
+						}
+					}
+				}}
+				action=""
+			>
+				{info?.map((items, index) => {
+					return (
+						<div key={index} className={items.stage === 'Nacimiento' ? st.bird : st.panel}>
+							{items.stage === 'Nacimiento' ? (
+								<></>
+							) : (
+								<>
+									<div className={st.idName}>{items.stage}</div>
+									<hr />
 									<br />
 									<br />
-									Peso final:
 									<br />
-									Fecha real:
-								</div>
-								<div className={st.ask}>
-									{items.approDate}
-									<br />
-									<br />
-									<input
-										defaultValue={items.weigth}
-										type="number"
-										name={info?.lifecycle[index].stage}
-									/>
-									<br />
-									<input defaultValue={items.date} type="date" name="date" />
-								</div>
-							</>
-						)}
+									<div className={st.titles}>
+										Fecha pronosticada:
+										<br />
+										<br />
+										Peso final:
+										<br />
+										Fecha real:
+									</div>
+									<div className={st.ask}>
+										{items.approDate}
+										<br />
+										<br />
+										<input defaultValue={items.weigth} type="number" name={info?.stage} />
+										<br />
+										<input defaultValue={items.date} type="date" name="date" />
+									</div>
+								</>
+							)}
 
-						{items.stage === 'Nacimiento' ? (
-							<></>
-						) : (
-							<div className={st.btn}>
-								{/* <Buttons
+							{items.stage === 'Nacimiento' ? (
+								<></>
+							) : (
+								<div className={st.btn}>
+									{/* <Buttons
                                     route='#'
                                     btnType='submit'
                                     btnIconText={faFloppyDisk}
                                     label='Hecho'
                                     direction='bottom'
                                 /> */}
-							</div>
-						)}
-						<button type="submit">Enviar</button>
-					</form>
-				);
-			})}
+								</div>
+							)}
+						</div>
+					);
+				})}{' '}
+				<div>
+					<button type="submit">Enviar</button>
+				</div>
+			</form>
 		</>
 	);
 }
