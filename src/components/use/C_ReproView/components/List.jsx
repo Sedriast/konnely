783914b066c7f79cdfd5395667ad30e4bbfd9e29
exSED <sedriast@gmail.com>
2022-10-04 17:26:57 -0500

    Repro

--- src/components/use/C_ReproView/components/List.jsx
+++ src/components/use/C_ReproView/components/List.jsx
@@ -1,11 +1,9 @@
 import { Cards } from './Cards/Cards';
 
-import { litter } from '../../0-GeneralComp/2-FakeData/reproductiveCycle';
-
 export function List({ litters }) {
 	return (
 		<>
-			{litter?.map((item, index) => (
+			{litters?.map((item, index) => (
 				<Cards key={index} id={item.id} editor={item.editors} stages={item.stages} />
 			))}
 		</>
--- src/components/use/F-Forms/EditReproData/components/Form/Form.jsx
+++ src/components/use/F-Forms/EditReproData/components/Form/Form.jsx
@@ -1,65 +1,36 @@
 import st from './Form.module.css';
 
-import { faFloppyDisk } from '@fortawesome/free-solid-svg-icons';
+import { faFloppyDisk, faPaperPlane } from '@fortawesome/free-solid-svg-icons';
 import { Buttons } from '../../../../0-GeneralComp/1-Buttons/Buttons';
 export function Form({ info }) {
 	return (
 		<>
 			<form
 				className={st.container}
-				onSubmit={(e) => {
-					e.preventDefault();
-					for (const element of e.target) {
-						if (element.name !== '') {
-							console.log(element.name + ': ' + element.value);
-						}
-					}
+				onSubmit={() => {
+					console.log('***** el que lo encuentre :V');
 				}}
 				action=""
 			>
-				{info?.map((items, index) => {
-					return (
-						<div key={index} className={items.stage === 'Nacimiento' ? st.bird : st.panel}>
-							{items.stage === 'Nacimiento' ? (
-								<></>
-							) : (
-								<>
-									<div className={st.idName}>{items.stage}</div>
-									<hr />
-									<br />
-									<br />
-									<br />
-									<div className={st.titles}>
-										Fecha pronosticada:
-										<br />
-										<br />
-										Peso final:
-										<br />
-										Fecha real:
-									</div>
-									<div className={st.ask}>
-										{items.approDate}
-										<br />
-										<br />
-										<input defaultValue={items.weigth} type="number" name={info?.stage} />
-										<br />
-										<input defaultValue={items.date} type="date" name="date" />
-									</div>
-								</>
-							)}
-						</div>
-					);
-				})}{' '}
-				<div className={st.btn}>
+				<div className={st.panel}>
+					<Inputs
+						value={basicData?.id}
+						leyend="Identificador"
+						name="id"
+						placeholder="Ingrese el identificador"
+						type="number"
+						inputmode="numeric"
+						handleChange={handleChange}
+					/>
+				</div>
+				<div className={st.submit}>
 					<Buttons
-						route="#"
-						btnType="submit"
-						btnIconText={faFloppyDisk}
-						label="Guardar cambios"
-						direction="top"
-						btnClick={() => {
-							window.history.back();
-						}}
+						direction="bottom"
+						label="Guardar"
+						btnType="Submit"
+						btnClick={handleSubmit}
+						route="/vitaeslist"
+						btnIconText={faPaperPlane}
 					/>
 				</div>
 			</form>
