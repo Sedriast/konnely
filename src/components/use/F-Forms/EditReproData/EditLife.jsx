52144e111bffd6ad33f2a0eefe06bbfb5b984b0d
exSED <sedriast@gmail.com>
2022-10-04 15:07:25 -0500

    Repro edit

---
+++ src/components/use/F-Forms/EditReproData/EditLife.jsx
@@ -0,0 +1,23 @@
+import st from './EditLife.module.css';
+
+import { newTreats } from '../../0-GeneralComp/0-StaticData/options';
+import { stages } from '../../0-GeneralComp/2-FakeData/liveCycle';
+
+import { Form } from './comoponents/Form/Form';
+import { LeftBottomMenu } from '../../0-GeneralComp/1-PanelButtons/LeftBottomMenu/LeftBottomMenu';
+
+export function EditLife() {
+	return (
+		<>
+			<LeftBottomMenu
+				backCancel={newTreats}
+				click={() => {
+					window.history.back();
+				}}
+			/>
+			<div className={st.optionContainer}>
+				<Form info={stages} />
+			</div>
+		</>
+	);
+}
