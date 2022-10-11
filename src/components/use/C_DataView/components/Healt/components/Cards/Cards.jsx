755d0ade2d577ef107f7b4f2ac3bd0eec45cee1a
exSED <sedriast@gmail.com>
2022-10-11 15:09:25 -0500

    Modal

--- src/components/use/C_DataView/components/Healt/components/Cards/Cards.jsx
+++ src/components/use/C_DataView/components/Healt/components/Cards/Cards.jsx
@@ -11,105 +11,113 @@ import { useState } from 'react';
 import { AddAudit } from '../../../../../../firebase/funtions/AddInformation';
 
 export function Cards({ id, uid, date, signs, diagnosis, tratament, result, professional }) {
-    const { user } = useAuth();
-    const [isOpenModal, openModal, closeModal] = useModal(false);
-    const [auditoria, setAuditoria] = useState('');
+	const { user } = useAuth();
+	const [isOpenModal, openModal, closeModal] = useModal(false);
+	const [auditoria, setAuditoria] = useState('');
 
-    return (
-        <div className={st.container} id={id}>
-            <div className={st.panelId}>Tratamiento {id}</div>
-            <div className={st.btnPanel}>
-                <div>
-                    <Buttons route='/EditTrats' label='Editar' direction='bottom' btnIconText={faPen} />
-                </div>
-                <div>
-                    <Buttons
-                        route='#'
-                        label='Eliminar'
-                        direction='bottom'
-                        btnIconText={faTrash}
-                        btnClick={() => {
-                            swal({
-                                title: '¿Desea eliminar este tratamiento?',
-                                icon: 'warning',
-                                buttons: ['No', 'Si'],
-                            }).then((respuesta) => {
-                                if (respuesta) {
-                                    openModal();
-                                }
-                            });
-                        }}
-                    />
-                </div>
-            </div>
-            <Modal isOpen={isOpenModal} closeModal={closeModal}>
-                {isOpenModal && (
-                    <>
-                        <input
-                            defaultValue={auditoria}
-                            type='text'
-                            placeholder='Porque desea eliminar este tratamiento'
-                            onChange={(e) => {
-                                setAuditoria(e.target.value);
-                            }}></input>
-                        <button
-                            onClick={(e) => {
-                                e.preventDefault();
-                                if (auditoria !== '') {
-                                    AddAudit({
-                                        razon: auditoria,
-                                        uidTratament: uid,
-                                        uidProfile: user.uid,
-                                        userNameProfile: user.displayName,
-                                        fecha: Date.now(),
-                                    });
-                                    setAuditoria('');
-                                }
-                                closeModal();
-                            }}>
-                            Enviar
-                        </button>
-                    </>
-                )}
-            </Modal>
-            <br />
-            <br />
-            <hr />
-            <br />
-            <br />
-            <br />
-            <div className={st.panelInfo}>
-                <div className={st.sp}>
-                    Fecha:
-                    <br />
-                    <div>{date}</div>
-                </div>
-                <div className={st.sp}>
-                    Sintomas:
-                    <br />
-                    <div>{signs}</div>
-                </div>
-                <div className={st.sp}>
-                    Diagnostico:
-                    <br />
-                    <div>{diagnosis}</div>
-                </div>
-                <div className={st.sp}>
-                    Tratamiento:
-                    <br />
-                    <div>{tratament}</div>
-                </div>
-                <div className={st.sp}>
-                    Resultados:
-                    <br />
-                    <div>{result}</div>
-                </div>
-                <div className={st.sp}>
-                    Nombre del profecional:
-                    <br />
-                    <div>{professional}</div>
-                </div>
-            </div>
-        </div>
-    );
+	return (
+		<div className={st.container} id={id}>
+			<div className={st.panelId}>Tratamiento {id}</div>
+			<div className={st.btnPanel}>
+				<div>
+					<Buttons route="/EditTrats" label="Editar" direction="bottom" btnIconText={faPen} />
+				</div>
+				<div>
+					<Buttons
+						route="#"
+						label="Eliminar"
+						direction="bottom"
+						btnIconText={faTrash}
+						btnClick={() => {
+							swal({
+								title: '¿Desea eliminar este tratamiento?',
+								icon: 'warning',
+								buttons: ['No', 'Si'],
+							}).then((respuesta) => {
+								if (respuesta) {
+									openModal();
+								}
+							});
+						}}
+					/>
+				</div>
+			</div>
+			<Modal isOpen={isOpenModal} closeModal={closeModal}>
+				{isOpenModal && (
+					<>
+						<br />
+						¿Porque desea eliminar este tratamiento?:
+						<br />
+						<br />
+						<textarea
+							className={st.inpModal}
+							defaultValue={auditoria}
+							type="text"
+							placeholder="Porque desea eliminar este tratamiento"
+							onChange={(e) => {
+								setAuditoria(e.target.value);
+							}}
+						></textarea>
+						<button
+							className={st.btnModal}
+							onClick={(e) => {
+								e.preventDefault();
+								if (auditoria !== '') {
+									AddAudit({
+										razon: auditoria,
+										uidTratament: uid,
+										uidProfile: user.uid,
+										userNameProfile: user.displayName,
+										fecha: Date.now(),
+									});
+									setAuditoria('');
+								}
+								closeModal();
+							}}
+						>
+							Enviar
+						</button>
+					</>
+				)}
+			</Modal>
+			<br />
+			<br />
+			<hr />
+			<br />
+			<br />
+			<br />
+			<div className={st.panelInfo}>
+				<div className={st.sp}>
+					Fecha:
+					<br />
+					<div>{date}</div>
+				</div>
+				<div className={st.sp}>
+					Sintomas:
+					<br />
+					<div>{signs}</div>
+				</div>
+				<div className={st.sp}>
+					Diagnostico:
+					<br />
+					<div>{diagnosis}</div>
+				</div>
+				<div className={st.sp}>
+					Tratamiento:
+					<br />
+					<div>{tratament}</div>
+				</div>
+				<div className={st.sp}>
+					Resultados:
+					<br />
+					<div>{result}</div>
+				</div>
+				<div className={st.sp}>
+					Nombre del profecional:
+					<br />
+					<div>{professional}</div>
+				</div>
+			</div>
+		</div>
+	);
 }
