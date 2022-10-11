da8e497cada5edfa9ec65e4c5d2de6bc6ec7c199
exSED <sedriast@gmail.com>
2022-10-11 15:25:57 -0500

    styles

--- src/components/use/0-GeneralComp/0-StaticData/options.js
+++ src/components/use/0-GeneralComp/0-StaticData/options.js
@@ -1,11 +1,11 @@
 import {
-    faGripVertical,
-    faBarcode,
-    faFileInvoice,
-    faUsers,
-    faWrench,
-    faXmark,
-    faPlus,
+	faGripVertical,
+	faBarcode,
+	faFileInvoice,
+	faUsers,
+	faWrench,
+	faXmark,
+	faPlus,
 } from '@fortawesome/free-solid-svg-icons';
 
 /*##################################################################################|
@@ -14,10 +14,10 @@ import {
 |																					|
 ####################################################################################*/
 export const newTreats = {
-    id: 0,
-    icon: faXmark,
-    path: '#',
-    label: 'Cancelar',
+	id: 0,
+	icon: faXmark,
+	path: '#',
+	label: 'Cancelar',
 };
 /*##################################################################################|
 |																					|
@@ -25,18 +25,18 @@ export const newTreats = {
 |																					|
 ####################################################################################*/
 export const viewsListOptions = [
-    {
-        id: 0,
-        icon: faXmark,
-        path: '#',
-        label: 'Cancelar',
-    },
-    {
-        id: 1,
-        icon: faPlus,
-        path: '#',
-        label: 'Nuevo conejo',
-    },
+	{
+		id: 0,
+		icon: faXmark,
+		path: '#',
+		label: 'Cancelar',
+	},
+	{
+		id: 1,
+		icon: faPlus,
+		path: '#',
+		label: 'Nuevo conejo',
+	},
 ];
 /*###################################################################################
 |																					|
@@ -44,36 +44,36 @@ export const viewsListOptions = [
 |																					|
 ####################################################################################*/
 export const optionsData = [
-    {
-        id: 0,
-        icon: faWrench,
-        label: 'Configuración',
-    },
-    {
-        id: 1,
-        icon: faUsers,
-        label: 'Administrar usuarios',
-    },
+	{
+		id: 0,
+		icon: faWrench,
+		label: 'Configuración',
+	},
+	{
+		id: 1,
+		icon: faUsers,
+		label: 'Administrar usuarios',
+	},
 ];
 export const optionsDataUser = [
-    {
-        id: 0,
-        icon: faWrench,
-        label: 'Configuración',
-    },
+	{
+		id: 0,
+		icon: faWrench,
+		label: 'Configuración',
+	},
 ];
 
 export const optionsDataAdmin = [
-    {
-        id: 0,
-        icon: faWrench,
-        label: 'Configuración',
-    },
-    {
-        id: 1,
-        icon: faUsers,
-        label: 'Administrar usuarios',
-    },
+	{
+		id: 0,
+		icon: faWrench,
+		label: 'Configuración',
+	},
+	{
+		id: 1,
+		icon: faUsers,
+		label: 'Administrar usuarios',
+	},
 ];
 /*###################################################################################
 |																					|
@@ -81,22 +81,22 @@ export const optionsDataAdmin = [
 |																					|
 ####################################################################################*/
 export const generalOptions = [
-    {
-        id: 0,
-        icon: faBarcode,
-        path: '/analitics',
-        label: 'Estadísticas',
-    },
-    {
-        id: 1,
-        icon: faGripVertical,
-        path: '/vitaeslist',
-        label: 'Hojas de vida',
-    },
-    {
-        id: 2,
-        icon: faFileInvoice,
-        path: '/record',
-        label: 'Historial',
-    },
+	{
+		id: 0,
+		icon: faBarcode,
+		path: '/dashboard',
+		label: 'Estadísticas',
+	},
+	{
+		id: 1,
+		icon: faGripVertical,
+		path: '/vitaeslist',
+		label: 'Hojas de vida',
+	},
+	{
+		id: 2,
+		icon: faFileInvoice,
+		path: '/record',
+		label: 'Historial',
+	},
 ];
