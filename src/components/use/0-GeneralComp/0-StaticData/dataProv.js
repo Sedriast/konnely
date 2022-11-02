a8f34ea92ab3560f9f89b6920f285a29fedfa559
JfAlonso <alonsofelipe8888@gmail.com>
2022-11-01 20:58:19 -0500

    Update:UserEdit

--- src/components/use/0-GeneralComp/0-StaticData/dataProv.js
+++ src/components/use/0-GeneralComp/0-StaticData/dataProv.js
@@ -1,22 +1,33 @@
+// Contexto improvisado de la información de un conejo
+
 export const recuperar = (id, info) => {
-	basicData.id = id;
-	basicData.info = info;
+    basicData.id = id;
+    basicData.info = info;
 };
 export const basicData = {
-	id: null,
-	info: {},
+    id: null,
+    info: {},
 };
 
+// Contexto improvisado de la información de un tratamiento
+
 export const recuperarTrataments = (info) => {
-	tratamentsData.info = info;
+    tratamentsData.info = info;
 };
 export const tratamentsData = {
-	info: null,
+    info: null,
+};
+
+export const recuperarUser = (info) => {
+    userData.info = info;
+};
+export const userData = {
+    info: null,
 };
 
 export const reproData = {
-	rechazos: '0',
-	muertes: '0',
-	vivos: '0',
-	partos: '0',
+    rechazos: '0',
+    muertes: '0',
+    vivos: '0',
+    partos: '0',
 };
--- src/components/use/A_User/components/ListUsers/components/UserCards.jsx
+++ src/components/use/A_User/components/ListUsers/components/UserCards.jsx
@@ -1,16 +1,25 @@
 import st from '../styles/UserCards.module.css';
 import { Buttons } from '../../../../0-GeneralComp/1-Buttons/Buttons';
+import { recuperarUser } from '../../../../0-GeneralComp/0-StaticData/dataProv';
 
-export function UserCards({ userEmail, userId, userName }) {
-	return (
-		<div className={st.container}>
-			<h1>{userName}</h1>
+export function UserCards({ userEmail, userName, user }) {
+    return (
+        <div className={st.container}>
+            <h1>{userName}</h1>
 
-			<div className={st.mid}>{userEmail}</div>
+            <div className={st.mid}>{userEmail}</div>
 
-			<div className={st.btnEdit}>
-				<Buttons route="/user" btnIconText="✏️" label="Editar" direction="bottom" />
-			</div>
-		</div>
-	);
+            <div className={st.btnEdit}>
+                <Buttons
+                    route='/user'
+                    btnIconText='✏️'
+                    label='Editar'
+                    direction='bottom'
+                    btnClick={() => {
+                        recuperarUser(user);
+                    }}
+                />
+            </div>
+        </div>
+    );
 }
