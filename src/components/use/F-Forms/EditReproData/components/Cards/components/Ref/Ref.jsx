08d84f65dd982f9409f9d30e249ace63d5845caf
exSED <sedriast@gmail.com>
2022-11-01 20:54:33 -0500

    styles

--- src/components/use/F-Forms/EditReproData/components/Cards/components/Ref/Ref.jsx
+++ src/components/use/F-Forms/EditReproData/components/Cards/components/Ref/Ref.jsx
@@ -95,31 +95,55 @@ export function Ref({ stage, handleChange, date }) {
 			return (
 				<div className={st.panel}>
 					<h3>{stage.title}</h3>
-					{date !== null && <h1>Fecha aproximada: {ApproximateRepro(date).birth}</h1>}
+					<br />
+					<div className={st.edit}>
+						{date !== null && (
+							<>
+								<h1>Fecha aproximada:</h1> {ApproximateRepro(date).birth}
+							</>
+						)}
+					</div>
+
 					<br />
 					<hr />
 					<br />
 
-					<div className={st.pt}>
-						<h1>Crias vivas: </h1>
-						<input name="lives" type="text" defaultValue={stage.lives} />
-					</div>
-					<div className={st.pt}>
-						<h1>Crias muertas: </h1>
-						<input name="deaths" type="text" defaultValue={stage.deaths} />
-					</div>
-					<div className={st.pt}>
-						<h1>Homogeneizados: </h1>
-						<input name="homogen" type="text" defaultValue={stage.homogen} />
-					</div>
-					<div className={st.pt}>
-						<h1>Crias totales: </h1>
-						<input name="total" type="text" defaultValue={stage.total} />
-					</div>
-
-					<div className={st.pt}>
-						<h1>Fecha: </h1>
-						<input name="DateParto" type="date" defaultValue={stage.date} onChange={handleChange} />
+					<div className={st.edit}>
+						<Inputs
+							value={stage.lives}
+							name="lives"
+							type="text"
+							leyend="Crias vivas"
+							handleChange={handleChange}
+						/>
+						<Inputs
+							value={stage.deaths}
+							name="deaths"
+							type="text"
+							leyend="Crias muertas"
+							handleChange={handleChange}
+						/>
+						<Inputs
+							value={stage.homogen}
+							name="homogen"
+							type="text"
+							leyend="Homogeneizados"
+							handleChange={handleChange}
+						/>
+						<Inputs
+							value={stage.total}
+							name="total"
+							type="text"
+							leyend="Crias totales"
+							handleChange={handleChange}
+						/>
+						<Inputs
+							value={stage.date}
+							name="DateParto"
+							type="date"
+							leyend="Fecha"
+							handleChange={handleChange}
+						/>
 					</div>
 				</div>
 			);
@@ -128,27 +152,40 @@ export function Ref({ stage, handleChange, date }) {
 			return (
 				<div className={st.panel}>
 					<h3>{stage.title}</h3>
-					{date !== null && <h1>Fecha aproximada: {ApproximateRepro(date).weaning}</h1>}
+					<br />
+					<div className={st.edit}>
+						{date !== null && (
+							<>
+								<h1>Fecha aproximada:</h1> {ApproximateRepro(date).weaning}
+							</>
+						)}
+					</div>
+
 					<br />
 					<hr />
 					<br />
 
-					<div className={st.pt}>
-						<h1>Crias destetadas: </h1>
-						<input name="WeanedPups" type="text" defaultValue={stage.WeanedPups} />
-					</div>
-					<div className={st.pt}>
-						<h1>Peso de la camada: </h1>
-						<input name="LitterWeight" type="text" defaultValue={stage.LitterWeight} />
-					</div>
-
-					<div className={st.pt}>
-						<h1>Fecha: </h1>
-						<input
+					<div className={st.edit}>
+						<Inputs
+							value={stage.WeanedPups}
+							name="WeanedPups"
+							type="text"
+							leyend="Crias destetadas"
+							handleChange={handleChange}
+						/>
+						<Inputs
+							value={stage.LitterWeight}
+							name="LitterWeight"
+							type="text"
+							leyend="Peso de la camada"
+							handleChange={handleChange}
+						/>
+						<Inputs
+							value={stage.DateDestete}
 							name="DateDestete"
 							type="date"
-							defaultValue={stage.DateDestete}
-							onChange={handleChange}
+							leyend="Fecha"
+							handleChange={handleChange}
 						/>
 					</div>
 				</div>
