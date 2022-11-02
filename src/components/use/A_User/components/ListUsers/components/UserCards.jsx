982065ac0e57ddc74f5fa23109915ffb4fb49949
exSED <sedriast@gmail.com>
2022-11-02 15:16:31 -0500

    Style

--- src/components/use/A_User/components/ListUsers/components/UserCards.jsx
+++ src/components/use/A_User/components/ListUsers/components/UserCards.jsx
@@ -1,25 +1,25 @@
 import st from './UserCards.module.css';
 import { Buttons } from '../../../../0-GeneralComp/1-Buttons/Buttons';
 import { recuperarUser } from '../../../../0-GeneralComp/0-StaticData/dataProv';
+import { faPenToSquare } from '@fortawesome/free-solid-svg-icons';
 
 export function UserCards({ userEmail, userName, user }) {
-    return (
-        <div className={st.container}>
-            <h1>{userName}</h1>
+	return (
+		<div className={st.container}>
+			<h1>{userName}</h1>
 
-            <div className={st.mid}>{userEmail}</div>
-
-            <div className={st.btnEdit}>
-                <Buttons
-                    route='/user'
-                    btnIconText='✏️'
-                    label='Editar'
-                    direction='bottom'
-                    btnClick={() => {
-                        recuperarUser(user);
-                    }}
-                />
-            </div>
-        </div>
-    );
+			<div className={st.mid}>{userEmail}</div>
+			<div>
+				<Buttons
+					route="/user"
+					label="editar"
+					direction="bottom"
+					btnIconText={faPenToSquare}
+					btnClick={() => {
+						recuperarUser(user);
+					}}
+				/>
+			</div>
+		</div>
+	);
 }
--- src/components/use/A_User/components/UserData/UserData.jsx
+++ src/components/use/A_User/components/UserData/UserData.jsx
@@ -2,6 +2,9 @@ import st from './UserData.module.css';
 
 import { useAuth } from '../../../../../context/AuthContext';
 import { Themes } from './conponents/Themes/Themes';
+import { Buttons } from '../../../0-GeneralComp/1-Buttons/Buttons';
+import { faPenToSquare } from '@fortawesome/free-solid-svg-icons';
+import { recuperarUser } from '../../../0-GeneralComp/0-StaticData/dataProv';
 
 export function UserData({ user_ }) {
 	const { user } = useAuth();
@@ -12,16 +15,23 @@ export function UserData({ user_ }) {
 				<div className={st.profilePhoto}>
 					<img src={user.photoURL} alt="" href="" />
 				</div>
+				<div>
+					<Buttons
+						route="/user"
+						label="editar"
+						direction="bottom"
+						btnIconText={faPenToSquare}
+						btnClick={() => {
+							recuperarUser(user);
+						}}
+					/>
+				</div>
+				<br />
 				<div className={st.paragraph}>
 					<div>{user.displayName}</div>
 					<div>{user.email}</div>
-					<p>
-						Otros datos del usuario
-						<br />
-						<br />
-						<br />
-						fadfadfafdsafaf
-					</p>
+					<br />
+					<p>Otros datos del usuario</p>
 				</div>
 				<Themes />
 			</div>
