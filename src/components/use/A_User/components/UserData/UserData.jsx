ed6b0a3f4da68967ac381c7553edf36f2ea6c0b3
exSED <sedriast@gmail.com>
2022-11-02 21:19:51 -0500

    Style

--- src/components/use/A_User/components/UserData/UserData.jsx
+++ src/components/use/A_User/components/UserData/UserData.jsx
@@ -1,40 +1,40 @@
-import st from './UserData.module.css';
+import st from "./UserData.module.css";
 
-import { useAuth } from '../../../../../context/AuthContext';
-import { Themes } from './conponents/Themes/Themes';
-import { Buttons } from '../../../0-GeneralComp/1-Buttons/Buttons';
-import { faPenToSquare } from '@fortawesome/free-solid-svg-icons';
-import { recuperarUser } from '../../../0-GeneralComp/0-StaticData/dataProv';
+import { useAuth } from "../../../../../context/AuthContext";
+import { Themes } from "./conponents/Themes/Themes";
+import { Buttons } from "../../../0-GeneralComp/1-Buttons/Buttons";
+import { faPenToSquare } from "@fortawesome/free-solid-svg-icons";
+import { recuperarUser } from "../../../0-GeneralComp/0-StaticData/dataProv";
 
 export function UserData({ user_ }) {
-	const { user } = useAuth();
+  const { user } = useAuth();
 
-	return (
-		<>
-			<div className={st.userData}>
-				<div className={st.profilePhoto}>
-					<img src={user.photoURL} alt="" href="" />
-				</div>
-				<div>
-					<Buttons
-						route="/user"
-						label="editar"
-						direction="bottom"
-						btnIconText={faPenToSquare}
-						btnClick={() => {
-							recuperarUser(user);
-						}}
-					/>
-				</div>
-				<br />
-				<div className={st.paragraph}>
-					<div>{user.displayName}</div>
-					<div>{user.email}</div>
-					<br />
-					<p>Otros datos del usuario</p>
-				</div>
-				<Themes />
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div className={st.userData}>
+        <div className={st.profilePhoto}>
+          <img src={user.photoURL} alt="" href="" />
+        </div>
+        <div>
+          <Buttons
+            route="/user"
+            label="editar"
+            direction="bottom"
+            btnIconText={faPenToSquare}
+            btnClick={() => {
+              recuperarUser(user);
+            }}
+          />
+        </div>
+        <br />
+        <div className={st.paragraph}>
+          <div>{user.displayName}</div>
+          <div>{user.email}</div>
+          <br />
+          <div>Otros datos del usuario</div>
+        </div>
+        <Themes />
+      </div>
+    </>
+  );
 }
