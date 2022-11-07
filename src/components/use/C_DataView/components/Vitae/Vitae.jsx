fc42743731e177a8c817a9e0a05e03078cb7f088
exSED <sedriast@gmail.com>
2022-11-06 22:12:16 -0500

    styles

--- src/components/use/C_DataView/components/Vitae/Vitae.jsx
+++ src/components/use/C_DataView/components/Vitae/Vitae.jsx
@@ -1,64 +1,67 @@
 /* eslint-disable array-callback-return */
-import st from './Vitae.module.css';
+import st from "./Vitae.module.css";
 
-import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
-import { basicData } from '../../../0-GeneralComp/0-StaticData/dataProv';
+import { basicData } from "../../../0-GeneralComp/0-StaticData/dataProv";
 
-import { LifeCycle } from './components/LifeCycle/LifeCycle';
-import { RabbitDataView } from './components/RabbitDataView/RabbitDataView';
-import { ReproductiveCycle } from './components/ReproductiveCycle/ReproductiveCycle';
-import { PanelData1 } from '../../../C_PrimaryView/component/PanelData1';
-import { QueriesSimple_ } from '../../../../firebase/funtions/GetInformation';
+import { LifeCycle } from "./components/LifeCycle/LifeCycle";
+import { RabbitDataView } from "./components/RabbitDataView/RabbitDataView";
+import { ReproductiveCycle } from "./components/ReproductiveCycle/ReproductiveCycle";
+import { PanelData1 } from "../../../C_PrimaryView/component/PanelData1";
+import { QueriesSimple_ } from "../../../../firebase/funtions/GetInformation";
 import {
-    DeathStadics,
-    HomogenStadics,
-    LableStadics,
-    LivesStadics,
-} from '../../../0-GeneralComp/0-Scripts/FormatStadics';
+  DeathStadics,
+  HomogenStadics,
+  LableStadics,
+  LivesStadics,
+} from "../../../0-GeneralComp/0-Scripts/FormatStadics";
 
 export function Vitae() {
-    const navigate = useNavigate();
-    const stadics = QueriesSimple_({
-        coleccion: 'reproductive',
-        parametro: 'uidMother',
-        busqueda: basicData.info.uid,
-    }).props.children;
+  const navigate = useNavigate();
+  const stadics = QueriesSimple_({
+    coleccion: "reproductive",
+    parametro: "uidMother",
+    busqueda: basicData.info.uid,
+  }).props.children;
 
-    useEffect(() => {
-        if (basicData.id === null) {
-            navigate('/vitaeslist');
-            return null;
-        }
-    }, [navigate, stadics]);
+  useEffect(() => {
+    if (basicData.id === null) {
+      navigate("/vitaeslist");
+      return null;
+    }
+  }, [navigate, stadics]);
 
-    return (
-        <>
-            {basicData.id !== null && (
-                <div className={st.panel}>
-                    <RabbitDataView stageId={basicData.id} />
-                    <hr />
-                    <br />
-                    <LifeCycle info={basicData.info} />
-                    <br />
-                    <br />
-                    <hr />
-                    <br />
-                    <br />
-                    <ReproductiveCycle />
-                    <br />
-                    <br />
-                    <br />
-                    <br />
-                    <PanelData1
-                        labels={LableStadics(stadics)}
-                        lives={LivesStadics(stadics)}
-                        deaths={DeathStadics(stadics)}
-                        homogen={HomogenStadics(stadics)}
-                    />
-                </div>
-            )}
-        </>
-    );
+  return (
+    <>
+      {basicData.id !== null && (
+        <div className={st.panel}>
+          <RabbitDataView stageId={basicData.id} />
+          <hr />
+          <br />
+          <LifeCycle info={basicData.info} />
+          <br />
+          <br />
+          <hr />
+          <br />
+          <br />
+          <ReproductiveCycle />
+          <br />
+          <br />
+          <hr />
+          <br />
+          <br />
+          <div className={st.stad}>
+            <PanelData1
+              labels={LableStadics(stadics)}
+              lives={LivesStadics(stadics)}
+              deaths={DeathStadics(stadics)}
+              homogen={HomogenStadics(stadics)}
+            />
+          </div>
+        </div>
+      )}
+    </>
+  );
 }
