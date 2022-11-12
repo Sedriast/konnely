e6bfb43c18d7cac361ce0f8c84258326100c8d2b
exSED <sedriast@gmail.com>
2022-11-12 13:46:25 -0500

    CR changes

--- src/components/use/A_User/components/ListUsers/ListUsers.jsx
+++ src/components/use/A_User/components/ListUsers/ListUsers.jsx
@@ -1,28 +1,36 @@
-import st from './ListUsers.module.css';
-import { UserCards } from './components/UserCards';
-import { SearchAll } from '../../../../firebase/funtions/GetInformation';
+import st from "./ListUsers.module.css";
+import { UserCards } from "./components/UserCards";
+import { SearchAll } from "../../../../firebase/funtions/GetInformation";
 
 export function ListUsers() {
-	const data = SearchAll({ coleccion: 'users' }).props.children;
-	return (
-		<>
-			<div className={st.panelSearchBar}>
-				<input placeholder="Buscar" />
-				<button type="submit" className={st.btnSearch} onClick={() => {}}>
-					🔎
-				</button>
-			</div>
-			<div className={st.container}>
-				<div className={st.panelList}>
-					{data[0].uid !== null && (
-						<>
-							{data.map((users, index) => (
-								<UserCards key={index} userName={users.user} userEmail={users.email} user={users} />
-							))}
-						</>
-					)}
-				</div>
-			</div>
-		</>
-	);
+  const data = SearchAll({ coleccion: "users" }).props.children;
+  return (
+    <>
+      <div className={st.panelSearchBar}>
+        <form onSubmit={(e) => {}}>
+          <input name="buscar" placeholder="Buscar" onChange={(e) => {}} />
+          <button className={st.btnSearch} type="submit">
+            🔎
+          </button>
+        </form>
+      </div>
+
+      <div className={st.container}>
+        <div className={st.panelList}>
+          {data[0].uid !== null && (
+            <>
+              {data.map((users, index) => (
+                <UserCards
+                  key={index}
+                  userName={users.user}
+                  userEmail={users.email}
+                  user={users}
+                />
+              ))}
+            </>
+          )}
+        </div>
+      </div>
+    </>
+  );
 }
