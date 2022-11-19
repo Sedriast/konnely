dfc43e8142cb0bb0406e407a32d7fecc7234a493
exSED <sedriast@gmail.com>
2022-11-19 13:50:44 -0500

    Invoice form

--- src/components/use/F-Forms/NewInvoice/component/Form/Form.jsx
+++ src/components/use/F-Forms/NewInvoice/component/Form/Form.jsx
@@ -5,13 +5,25 @@ import { RabitList } from "./RabitLists/RabitList";
 
 export function Form() {
   const [rabitList, setRabitList] = useState([
-    { id: 1, name: "Rabit 1", race: "RabbitNS" },
-    { id: 1, name: "Rabit 1", race: "RabbitNS" },
-    { id: 1, name: "Rabit 1", race: "RabbitNS" },
+    { id: 1, name: "Rabit 1", race: "RabbitNS", weith: 5 },
+    { id: 1, name: "Rabit 1", race: "RabbitNS", weith: 6 },
+    { id: 1, name: "Rabit 1", race: "RabbitNS", weith: 7 },
   ]);
   const cm = (
     <>
-      <form className={st.panelContainer} action="">
+      <form
+        className={st.panelContainer}
+        onSubmit={(e) => {
+          e.preventDefault();
+          let aux = {};
+          for (const element of e.target) {
+            if (element.name !== "") {
+              aux = { ...aux, [element.name]: element.value };
+            }
+          }
+          console.log(aux);
+        }}
+      >
         <div className={st.headerInvo}>
           <div className={st.headerInvoLeft}>
             <p>Unidad agroambiental el Tíbar</p>
@@ -25,37 +37,99 @@ export function Form() {
               <input name="numInvo" id="numInvo" type="text" />
             </p>
             <br />
-            <p>Fecha: {new Date().toLocaleDateString()}</p>
+            <p className={st.pa}>
+              Fecha:
+              <input
+                className={st.nf}
+                name="date"
+                id="date"
+                type="text"
+                disabled
+                value={new Date().toLocaleDateString()}
+              />
+            </p>
           </div>
         </div>
         <hr />
         <div className={st.bodyInvo}>
           {/* Lista de los conejos activos existentes */}
           <RabitList />
-          <table>
-            <tr>
-              <th>Id conejo</th>
-              <th>Genero</th>
-              <th>Raza</th>
-              <th>Precio</th>
-            </tr>
-            {rabitList.map((rabit, index) => {
-              return (
-                <tr>
-                  <th>{index}</th>
-                  <td>{rabit.id}</td>
-                  <td>{rabit.name}</td>
-                  <td>{rabit.race}</td>
-                  <td>
-                    <input type="text" />
-                  </td>
-                </tr>
-              );
-            })}
-          </table>
+          <div className={st.dataInvoice}>
+            <table>
+              <tr>
+                <th></th>
+                <th>Id conejo</th>
+                <th>Genero</th>
+                <th>Raza</th>
+                <th>Peso</th>
+                <th>Precio</th>
+              </tr>
+              {rabitList.map((rabit, index) => {
+                return (
+                  <tr>
+                    <th>{index + 1}</th>
+                    <td>
+                      <input
+                        disabled
+                        className={st.nf}
+                        name={"rid_" + index}
+                        id={"rid_" + index}
+                        type="text"
+                        value={rabit.id}
+                      />
+                    </td>
+                    <td>
+                      <input
+                        className={st.nf}
+                        disabled
+                        name={"rgen_" + index}
+                        id={"rgen_" + index}
+                        type="text"
+                        value={rabit.name}
+                      />
+                    </td>
+                    <td>
+                      <input
+                        className={st.nf}
+                        disabled
+                        name={"rrace_" + index}
+                        id={"rrace_" + index}
+                        type="text"
+                        value={rabit.race}
+                      />
+                    </td>
+                    <td>
+                      <input
+                        className={st.nf}
+                        disabled
+                        name={"rweith_" + index}
+                        id={"rweith_" + index}
+                        type="text"
+                        value={rabit.weith}
+                      />
+                    </td>
+                    <td>
+                      <input type="text" />
+                    </td>
+                  </tr>
+                );
+              })}
+            </table>
+          </div>
         </div>
+
+        <br />
         <hr />
-        <div className={st.footerInvo}></div>
+        <div></div>
+        <div className={st.footerInvo}>
+          <br />
+          <p>Total factura</p>
+          <br />
+          <br />
+          <div>
+            <button type="submit">✔️✔️</button>
+          </div>
+        </div>
       </form>
     </>
   );
