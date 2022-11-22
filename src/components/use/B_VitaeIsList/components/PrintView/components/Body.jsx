f4886291c0dbf73fa1ab83424692b51933189994
JfAlonso <alonsofelipe8888@gmail.com>
2022-11-22 10:01:12 -0500

    Update:Body

--- src/components/use/B_VitaeIsList/components/PrintView/components/Body.jsx
+++ src/components/use/B_VitaeIsList/components/PrintView/components/Body.jsx
@@ -1,75 +1,75 @@
-import st from "./Body.module.css";
+import st from './Body.module.css';
 
 export function Body({ dataFemale }) {
-  const cm = (
-    <div className={st.container}>
-      <div className={st.carac}>
-        <table>
-          <thead>
-            <th>Especie Cunicola</th>
-          </thead>
-          <tbody>
-            <table>
-              <thead>
-                <th>
-                  Identificador
-                  <br />
-                  de la hembras
-                </th>
-                <th>Fecha nacimiento</th>
-                <th>Raza</th>
-                <th>Edad</th>
-                <th>Peso</th>
-                <th>Procedencia</th>
-              </thead>
-              <tbody>
-                <td>{dataFemale?.id}</td>
-                <td>{dataFemale?.date}</td>
-                <td>{dataFemale?.race}</td>
-                <td>23</td>
-                <td>{dataFemale?.weigth}</td>
-                <td>{dataFemale?.id}</td>
-                <td>{dataFemale?.id}</td>
-              </tbody>
-            </table>{" "}
-          </tbody>
-        </table>
-      </div>
-      <table className={st.tableTop}>
-        <thead>
-          <th>Fecha servicio</th>
-          <th>Placa macho</th>
-          <th>
-            Fecha detección
-            <br />
-            de preñes
-          </th>
-          <th>
-            Fecha
-            <br />
-            posible parto
-          </th>
-          <th>
-            Fecha
-            <br />
-            atención de parto
-          </th>
-          <th>
-            Fecha real
-            <br />
-            de parto
-          </th>
-          <th>Crias vivas</th>
-          <th>Crias muertas</th>
-          <th>Peso nacimiento (gr)</th>
-          <th>Fecha destete</th>
-          <th>Crias hembras</th>
-          <th>Crias machos</th>
-          <th>Peso destete</th>
-        </thead>
-        <tbody></tbody>
-      </table>
-    </div>
-  );
-  return cm;
+    const cm = (
+        <div className={st.container}>
+            <div className={st.carac}>
+                <table>
+                    <thead>
+                        <th>Especie Cunicola</th>
+                    </thead>
+                    <tbody>
+                        <table>
+                            <thead>
+                                <th>
+                                    Identificador
+                                    <br />
+                                    de la hembras
+                                </th>
+                                <th>Fecha nacimiento</th>
+                                <th>Raza</th>
+                                <th>Edad</th>
+                                <th>Peso</th>
+                                <th>Procedencia</th>
+                            </thead>
+                            <tbody>
+                                <td>{dataFemale?.id}</td>
+                                <td>{dataFemale?.date}</td>
+                                <td>{dataFemale?.race}</td>
+                                <td>23</td>
+                                <td>{dataFemale?.weigth}</td>
+                                <td>{dataFemale?.id}</td>
+                                <td>{dataFemale?.id}</td>
+                            </tbody>
+                        </table>{' '}
+                    </tbody>
+                </table>
+            </div>
+            <table className={st.tableTop}>
+                <thead>
+                    <th>Fecha servicio</th>
+                    <th>Placa macho</th>
+                    <th>
+                        Fecha detección
+                        <br />
+                        de preñes
+                    </th>
+                    <th>
+                        Fecha
+                        <br />
+                        posible parto
+                    </th>
+                    <th>
+                        Fecha
+                        <br />
+                        atención de parto
+                    </th>
+                    <th>
+                        Fecha real
+                        <br />
+                        de parto
+                    </th>
+                    <th>Crias vivas</th>
+                    <th>Crias muertas</th>
+                    <th>Peso nacimiento (gr)</th>
+                    <th>Fecha destete</th>
+                    <th>Crias hembras</th>
+                    <th>Crias machos</th>
+                    <th>Peso destete</th>
+                </thead>
+                <tbody></tbody>
+            </table>
+        </div>
+    );
+    return cm;
 }
