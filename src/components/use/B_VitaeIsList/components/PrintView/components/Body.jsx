ff63c9f886aa9ae7b2addb01dc0eb73ed8767cd0
exSED <sedriast@gmail.com>
2022-11-22 12:34:38 -0500

    Tratamientos

--- src/components/use/B_VitaeIsList/components/PrintView/components/Body.jsx
+++ src/components/use/B_VitaeIsList/components/PrintView/components/Body.jsx
@@ -1,171 +1,124 @@
-import st from './Body.module.css';
-import { Data } from './Data/Data';
-import { basicData } from '../../../../0-GeneralComp/0-StaticData/dataProv';
+import st from "./Body.module.css";
+import { Data } from "./Data/Data";
+import { basicData } from "../../../../0-GeneralComp/0-StaticData/dataProv";
 
 export function Body({ cicles }) {
-    const dataCicle = (cicle) => {
-        console.log(cicle);
-        const data = [
-            cicle?.stages[0]?.date,
-            cicle?.stages[0]?.male,
-            cicle?.stages[1]?.date,
-            cicle?.stages[3]?.approximateDate,
-            cicle?.stages[2]?.date,
-            cicle?.stages[3]?.date,
-            cicle?.stages[3]?.lives,
-            cicle?.stages[3]?.deaths,
-            '--------',
-            cicle?.stages[4]?.date,
-            cicle?.stages[4]?.FemaleHatchlings,
-            cicle?.stages[4]?.MaleHatchlings,
-            cicle?.stages[4]?.LitterWeight,
-        ];
-        return data;
-    };
-    const cm = (
-        <div className={st.container}>
-            <div className={st.carac}>
-                <table>
-                    <thead>
-                        <th>Especie Cunicola</th>
-                    </thead>
-                    <tbody>
-                        <table>
-                            <thead>
-                                <th>
-                                    Identificador
-                                    <br />
-                                    de la hembra
-                                </th>
-                                <th>Fecha nacimiento</th>
-                                <th>Raza</th>
-                                <th>Edad</th>
-                                <th>Peso</th>
-                                <th>Procedencia</th>
-                            </thead>
-                            <tbody>
-                                <td>{basicData?.info?.id}</td>
-                                <td>{basicData?.info?.nacimiento}</td>
-                                <td>{basicData?.info?.raza}</td>
-                                <td>23</td>
-                                <td>{basicData?.info?.weigth}</td>
-                                <td>{basicData?.info?.origen}</td>
-                            </tbody>
-                        </table>
-                    </tbody>
-                </table>
-            </div>
-            <table className={st.tableTop}>
-                <thead>
-                    <th>Fecha servicio</th>
-                    <th>Placa macho</th>
-                    <th>
-                        Fecha detección
-                        <br />
-                        de preñes
-                    </th>
-                    <th>
-                        Fecha
-                        <br />
-                        posible parto
-                    </th>
-                    <th>
-                        Fecha
-                        <br />
-                        atención de parto
-                    </th>
-                    <th>
-                        Fecha real
-                        <br />
-                        de parto
-                    </th>
-                    <th>Crias vivas</th>
-                    <th>Crias muertas</th>
-                    <th>Peso nacimiento (gr)</th>
-                    <th>Fecha destete</th>
-                    <th>Crias hembras</th>
-                    <th>Crias machos</th>
-                    <th>Peso destete</th>
-                </thead>
-                <tbody>
-                    {cicles?.map((e) => {
-                        if (e.state === false) return <Data data={dataCicle(e)} />;
-                        return <></>;
-                    })}
-                </tbody>
+  const dataCicle = (cicle) => {
+    console.log(cicle);
+    const data = [
+      cicle?.stages[0]?.date,
+      cicle?.stages[0]?.male,
+      cicle?.stages[1]?.date,
+      cicle?.stages[3]?.approximateDate,
+      cicle?.stages[2]?.date,
+      cicle?.stages[3]?.date,
+      cicle?.stages[3]?.lives,
+      cicle?.stages[3]?.deaths,
+      "--------",
+      cicle?.stages[4]?.date,
+      cicle?.stages[4]?.FemaleHatchlings,
+      cicle?.stages[4]?.MaleHatchlings,
+      cicle?.stages[4]?.LitterWeight,
+    ];
+    return data;
+  };
+  const cm = (
+    <div className={st.container}>
+      <div className={st.carac}>
+        <table>
+          <thead>
+            <th>Especie Cunicola</th>
+          </thead>
+          <tbody>
+            <table>
+              <thead>
+                <th>
+                  Identificador
+                  <br />
+                  de la hembra
+                </th>
+                <th>Fecha nacimiento</th>
+                <th>Raza</th>
+                <th>Edad</th>
+                <th>Peso</th>
+                <th>Procedencia</th>
+              </thead>
+              <tbody>
+                <td>{basicData?.info?.id}</td>
+                <td>{basicData?.info?.nacimiento}</td>
+                <td>{basicData?.info?.raza}</td>
+                <td>23</td>
+                <td>{basicData?.info?.weigth}</td>
+                <td>{basicData?.info?.origen}</td>
+              </tbody>
             </table>
-        </div>
-    );
-    return cm;
-    // const cmu = (
-    //     <div className={st.container}>
-    //         <div className={st.carac}>
-    //             <table>
-    //                 <thead>
-    //                     <th>Especie Cunicola</th>
-    //                 </thead>
-    //                 <tbody>
-    //                     <table>
-    //                         <thead>
-    //                             <th>
-    //                                 Identificador
-    //                                 <br />
-    //                                 de la hembras
-    //                             </th>
-    //                             <th>Fecha nacimiento</th>
-    //                             <th>Raza</th>
-    //                             <th>Edad</th>
-    //                             <th>Peso</th>
-    //                             <th>Procedencia</th>
-    //                         </thead>
-    //                         <tbody>
-    //                             <td>{dataFemale?.id}</td>
-    //                             <td>{dataFemale?.date}</td>
-    //                             <td>{dataFemale?.race}</td>
-    //                             <td>23</td>
-    //                             <td>{dataFemale?.weigth}</td>
-    //                             <td>{dataFemale?.id}</td>
-    //                             <td>{dataFemale?.id}</td>
-    //                         </tbody>
-    //                     </table>{' '}
-    //                 </tbody>
-    //             </table>
-    //         </div>
-    //         <table className={st.tableTop}>
-    //             <thead>
-    //                 <th>Fecha servicio</th>
-    //                 <th>Placa macho</th>
-    //                 <th>
-    //                     Fecha detección
-    //                     <br />
-    //                     de preñes
-    //                 </th>
-    //                 <th>
-    //                     Fecha
-    //                     <br />
-    //                     posible parto
-    //                 </th>
-    //                 <th>
-    //                     Fecha
-    //                     <br />
-    //                     atención de parto
-    //                 </th>
-    //                 <th>
-    //                     Fecha real
-    //                     <br />
-    //                     de parto
-    //                 </th>
-    //                 <th>Crias vivas</th>
-    //                 <th>Crias muertas</th>
-    //                 <th>Peso nacimiento (gr)</th>
-    //                 <th>Fecha destete</th>
-    //                 <th>Crias hembras</th>
-    //                 <th>Crias machos</th>
-    //                 <th>Peso destete</th>
-    //             </thead>
-    //             <tbody></tbody>
-    //         </table>
-    //     </div>
-    // );
-    // return cmu;
+          </tbody>
+        </table>
+      </div>
+      <div className={st.complete}>
+        <table className={st.tableTop}>
+          <thead>
+            <th>Fecha servicio</th>
+            <th>Placa macho</th>
+            <th>
+              Fecha detección
+              <br />
+              de preñes
+            </th>
+            <th>
+              Fecha
+              <br />
+              posible parto
+            </th>
+            <th>
+              Fecha
+              <br />
+              atención de parto
+            </th>
+            <th>
+              Fecha real
+              <br />
+              de parto
+            </th>
+            <th>Crias vivas</th>
+            <th>Crias muertas</th>
+            <th>Peso nacimiento (gr)</th>
+            <th>Fecha destete</th>
+            <th>Crias hembras</th>
+            <th>Crias machos</th>
+            <th>Peso destete</th>
+          </thead>
+          <tbody>
+            {cicles?.map((e) => {
+              if (e.state === false) return <Data data={dataCicle(e)} />;
+              return <></>;
+            })}
+          </tbody>
+        </table>
+      </div>
+
+      <div className={st.carac}>
+        <table>
+          <thead>
+            <tr>
+              <th>Fecha</th>
+              <th>sintomas,signos,vacunas </th>
+              <th>Diagnostico</th>
+              <th>Medicamento</th>
+              <th>Dosis</th>
+              <th>Redultados</th>
+              <th>Nombre del profecional</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* {cicles?.map((e) => {
+            if (e.state === false) return <Data data={dataCicle(e)} />;
+            return <></>;
+          })} */}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+  return cm;
 }
--- src/components/use/C_ReproView/components/Cards/Cards.jsx
+++ src/components/use/C_ReproView/components/Cards/Cards.jsx
@@ -1,45 +1,50 @@
-import st from './Cards.module.css';
+import st from "./Cards.module.css";
 
-import { faPenToSquare } from '@fortawesome/free-solid-svg-icons';
-import { Buttons } from '../../../0-GeneralComp/1-Buttons/Buttons';
+import { faPenToSquare } from "@fortawesome/free-solid-svg-icons";
+import { Buttons } from "../../../0-GeneralComp/1-Buttons/Buttons";
 
-import { Ref } from './components/Ref';
+import { Ref } from "../../../C_DataView/components/Vitae/components/ReproductiveCycle/components/Cards/components/Ref";
 
 export function Cards({ editor, stages }) {
-    return (
-        <div className={st.container}>
-            {editor?.length === 1 ? (
-                <>
-                    <div className={st.panelId}>Editor: {editor}</div>
-                </>
-            ) : (
-                <>
-                    <div className={st.panelId}>
-                        Editores:
-                        {editor?.map((u) => (
-                            <h1> {'* ' + u}</h1>
-                        ))}
-                    </div>
-                </>
-            )}
-            <div className={st.panelId}>
-                <div className={st.btnEdit}>
-                    <Buttons route='/formEditRepro' label='Editar' direction='bottom' btnIconText={faPenToSquare} />
-                </div>
-            </div>
-            <br />
-            <div className={st.panelInfo}>
-                {stages?.map((element, index) => {
-                    return (
-                        <>
-                            <Ref key={index} stage={element} />
-                            {element.title === 'Destete' ? '' : '>'}
-                        </>
-                    );
-                })}
-            </div>
-            <br />
-            <br />
+  return (
+    <div className={st.container}>
+      {editor?.length === 1 ? (
+        <>
+          <div className={st.panelId}>Editor: {editor}</div>
+        </>
+      ) : (
+        <>
+          <div className={st.panelId}>
+            Editores:
+            {editor?.map((u) => (
+              <h1> {"* " + u}</h1>
+            ))}
+          </div>
+        </>
+      )}
+      <div className={st.panelId}>
+        <div className={st.btnEdit}>
+          <Buttons
+            route="/formEditRepro"
+            label="Editar"
+            direction="bottom"
+            btnIconText={faPenToSquare}
+          />
         </div>
-    );
+      </div>
+      <br />
+      <div className={st.panelInfo}>
+        {stages?.map((element, index) => {
+          return (
+            <>
+              <Ref key={index} stage={element} />
+              {element.title === "Destete" ? "" : ">"}
+            </>
+          );
+        })}
+      </div>
+      <br />
+      <br />
+    </div>
+  );
 }
