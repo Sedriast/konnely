b57ed09540b6c019220899a793af350afc84ac17
exSED <sedriast@gmail.com>
2022-11-23 12:11:49 -0500

    Style

--- src/components/use/B_VitaeIsList/components/PrintView/PrintView.jsx
+++ src/components/use/B_VitaeIsList/components/PrintView/PrintView.jsx
@@ -11,6 +11,9 @@ import s from "../../../../img/LogoUDEC.png";
 
 import { useReactToPrint } from "react-to-print";
 import { useRef } from "react";
+import { Buttons } from "../../../0-GeneralComp/1-Buttons/Buttons";
+
+import { faPrint } from "@fortawesome/free-solid-svg-icons";
 
 export function PrintView() {
   const componentRef = useRef();
@@ -38,74 +41,78 @@ export function PrintView() {
           window.history.back();
         }}
       />
-      <button className={st.btnPrint} onClick={print}>
-        Imprimir
-      </button>
+      
+      <div className={st.btnPrint}>
+        <Buttons label="Imprimir" direction="rigth" btnClick={print} route="#" btnIconText={faPrint}/>
+      </div>
       <div className={st.optionContainer} ref={componentRef}>
-        <div className={st.headPi}>
-          <div className={st.con}>
-            <div>
-              <img src={s} alt="" />
-            </div>
-            <table>
-              <thead>
-                <tr>
+        <div className={st.containerObj}>  
+          <div className={st.headPi}>
+            <div className={st.con}>
+              <div>
+                <img src={s} alt="" />
+              </div>
+              <table>
+                <thead>
                   <tr>
-                    <th>MARCO PROCESO DE APOYO</th>
-                  </tr>
-                  <tr>
-                    <th>PROCESO GÉSTION DE PROCESO ACADÉMICO</th>
-                  </tr>
-                  <tr>
-                    <th>HOJA DE VIDA SEMOVIENTE HEMBRA</th>
-                  </tr>
-                  <th>
                     <tr>
-                      <th>CÓDIGO: AAAr031</th>
+                      <th>MARCO PROCESO DE APOYO</th>
                     </tr>
                     <tr>
-                      <th>VERSIÓN: 7</th>
+                      <th>PROCESO GÉSTION DE PROCESO ACADÉMICO</th>
+                    </tr>
+                    <tr>
+                      <th>HOJA DE VIDA SEMOVIENTE HEMBRA</th>
                     </tr>
                     <th>
-                      FECHA:
-                      {window
-                        .Date()
-                        .replace(" GMT-0500 (hora estándar de Colombia)", "")}
+                      <tr>
+                        <th>CÓDIGO: AAAr031</th>
+                      </tr>
+                      <tr>
+                        <th>VERSIÓN: 7</th>
+                      </tr>
+                      <th>
+                        FECHA:
+                        {window
+                          .Date()
+                          .replace(" GMT-0500 (hora estándar de Colombia)", "")}
+                      </th>
                     </th>
-                  </th>
-                </tr>
-              </thead>
-            </table>
-          </div>
+                  </tr>
+                </thead>
+              </table>
+            </div>
 
-          <br />
-          <hr />
-          <br />
-          <br />
-        </div>
-        <Body cicles={cicles} trataments={trataments} />
-        <div className={st.foot}>
-          <br />
-          <br />
-          <hr />
-          <br />
-          <br />
-          <br />
-          <p>
-            Calle 6 N° 9-80 Ubate – Cundinamarca Teléfono (091) 855 3055/3056
-          </p>
-          <p>Ext.127 Línea Gratuita 018000180414</p>
-          <p>
-            {" "}
-            <a href="www.ucundinamarca.edu.co">
-              www.ucundinamarca.edu.co{" "}
-            </a>{" "}
-            E-mail:{" "}
-            <a href="info@ucundinamarca.edu.co">info@ucundinamarca.edu.co</a>
-          </p>
-          <p>NIT: 890.680.062-2</p>
+            <br />
+            <hr />
+            <br />
+            <br />
+          </div>
+          <Body cicles={cicles} trataments={trataments} />
+          <div className={st.foot}>
+            <br />
+            <br />
+            <hr />
+            <br />
+            <br />
+            <br />
+            <p>
+              Calle 6 N° 9-80 Ubate – Cundinamarca Teléfono (091) 855 3055/3056
+            </p>
+            <p>Ext.127 Línea Gratuita 018000180414</p>
+            <p>
+              {" "}
+              <a href="www.ucundinamarca.edu.co">
+                www.ucundinamarca.edu.co{" "}
+              </a>{" "}
+              E-mail:{" "}
+              <a href="info@ucundinamarca.edu.co">info@ucundinamarca.edu.co</a>
+            </p>
+            <p>NIT: 890.680.062-2</p>
+          </div>
         </div>
       </div>
+
     </>
   );
   return cm;
