f86f2f3b1c9d78c26fedbaf52f5b9523090147fe
JfAlonso <alonsofelipe8888@gmail.com>
2022-11-23 12:35:50 -0500

    Update:Search

--- src/components/use/C_ReproView/ReproView.jsx
+++ src/components/use/C_ReproView/ReproView.jsx
@@ -4,23 +4,54 @@ import { newTreats } from '../0-GeneralComp/0-StaticData/options';
 
 import { LeftBottomMenu } from '../0-GeneralComp/1-PanelButtons/LeftBottomMenu/LeftBottomMenu';
 import { List } from './components/List';
+import { useState } from 'react';
+import { basicData } from '../0-GeneralComp/0-StaticData/dataProv';
+import { QueriesSimple_ } from '../../firebase/funtions/GetInformation';
 
 export function ReproView() {
-	return (
-		<>
-			<LeftBottomMenu
-				backCancel={newTreats}
-				click={() => {
-					window.history.back();
-				}}
-			/>
-			<div className={st.panelSearchBar}>
-				<input placeholder="Buscar" onChange={(e) => {}} />
-				<button className={st.btnSearch}>🔎</button>
-			</div>
-			<div className={st.optionContainer}>
-				<List />
-			</div>
-		</>
-	);
+    const [search, setSearch] = useState('');
+    const reproductiveCycles = QueriesSimple_({
+        coleccion: 'reproductive',
+        parametro: 'uidMother',
+        busqueda: basicData.info.uid,
+    }).props.children;
+    console.log(reproductiveCycles);
+    function buscar(e) {
+        const valor = reproductiveCycles.filter(function (element) {
+            if (
+                element.signs.toString().toLowerCase().includes(e.toLowerCase()) ||
+                element.date.toString().toLowerCase().includes(e.toLowerCase()) ||
+                element.diagnosis.toString().toLowerCase().includes(e.toLowerCase())
+            )
+                return element;
+            return null;
+        });
+        setSearch(valor);
+    }
+    return (
+        <>
+            <LeftBottomMenu
+                backCancel={newTreats}
+                click={() => {
+                    window.history.back();
+                }}
+            />
+            <div className={st.panelSearchBar}>
+                <input
+                    placeholder='Buscar'
+                    onChange={(e) => {
+                        buscar(e.target.value);
+                    }}
+                />
+                <button className={st.btnSearch}>🔎</button>
+            </div>
+            <div className={st.optionContainer}>
+                {search !== '' ? (
+                    <List reproductiveCycles={reproductiveCycles} />
+                ) : (
+                    <List reproductiveCycles={search} />
+                )}
+            </div>
+        </>
+    );
 }
--- src/components/use/C_ReproView/components/List.jsx
+++ src/components/use/C_ReproView/components/List.jsx
@@ -1,38 +1,31 @@
-import st from "../ReproView.module.css";
+import st from '../ReproView.module.css';
 
-import { QueriesSimple_ } from "../../../firebase/funtions/GetInformation";
-import { basicData } from "../../0-GeneralComp/0-StaticData/dataProv";
-import { Cards } from "../../C_DataView/components/Vitae/components/ReproductiveCycle/components/Cards/Cards";
+import { Cards } from '../../C_DataView/components/Vitae/components/ReproductiveCycle/components/Cards/Cards';
 
-export function List() {
-  const reproductiveCycles = QueriesSimple_({
-    coleccion: "reproductive",
-    parametro: "uidMother",
-    busqueda: basicData.info.uid,
-  }).props.children;
-  return (
-    <>
-      {reproductiveCycles?.length !== 0 ? (
+export function List({ reproductiveCycles }) {
+    return (
         <>
-          {reproductiveCycles?.map(
-            (item, index) =>
-              reproductiveCycles[index]?.state === false && (
-                <Cards
-                  key={index}
-                  id={item.id}
-                  editor={item.displayNameEditors}
-                  stages={item.stages}
-                  uidMother={item.uidMother}
-                  item={item}
-                />
-              )
-          )}
+            {reproductiveCycles?.length !== 0 ? (
+                <>
+                    {reproductiveCycles?.map(
+                        (item, index) =>
+                            reproductiveCycles[index]?.state === false && (
+                                <Cards
+                                    key={index}
+                                    id={item.id}
+                                    editor={item.displayNameEditors}
+                                    stages={item.stages}
+                                    uidMother={item.uidMother}
+                                    item={item}
+                                />
+                            )
+                    )}
+                </>
+            ) : (
+                <>
+                    <div className={st.des}>No existen registros</div>
+                </>
+            )}
         </>
-      ) : (
-        <>
-          <div className={st.des}>No existen registros</div>
-        </>
-      )}
-    </>
-  );
+    );
 }
