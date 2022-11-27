a7136f7a82e715cdbe8564bf358c52152b4ca963
JfAlonso <alonsofelipe8888@gmail.com>
2022-11-27 10:28:54 -0500

    Update:tr

--- src/components/use/C_DataView/components/Vitae/components/ReproductiveCycle/components/Cards/Cards.jsx
+++ src/components/use/C_DataView/components/Vitae/components/ReproductiveCycle/components/Cards/Cards.jsx
@@ -1,73 +1,73 @@
-import st from "./Cards.module.css";
+import st from './Cards.module.css';
 
-import { faPenToSquare, faTrash } from "@fortawesome/free-solid-svg-icons";
-import { Buttons } from "../../../../../../../0-GeneralComp/1-Buttons/Buttons";
+import { faPenToSquare, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { Buttons } from '../../../../../../../0-GeneralComp/1-Buttons/Buttons';
 
-import { Ref } from "./components/Ref";
-import swal from "sweetalert";
-import { RemovalCamada } from "../../../../../../../../firebase/funtions/AddInformation";
-import { recuperarCamada } from "../../../../../../../0-GeneralComp/0-StaticData/dataProv";
+import { Ref } from './components/Ref';
+import swal from 'sweetalert';
+import { RemovalCamada } from '../../../../../../../../firebase/funtions/AddInformation';
+import { recuperarCamada } from '../../../../../../../0-GeneralComp/0-StaticData/dataProv';
 
 export function Cards({ stages, item }) {
-	const cm = (
-		<div className={st.container}>
-			<div className={st.btnEdit}>
-				<div>
-					<Buttons
-						route='/formEditRepro'
-						label='Editar'
-						direction='bottom'
-						btnIconText={faPenToSquare}
-						btnClick={() => {
-							recuperarCamada(item.uid);
-						}}
-					/>
-				</div>
-				<div>
-					<Buttons
-						route='#'
-						label='Borrar'
-						direction='bottom'
-						btnIconText={faTrash}
-						btnClick={() => {
-							swal({
-								title: "¿Esta seguro que desea eliminar esta cuenta?",
-								text: "Una vez eliminada no podrá recuperarla",
-								dangerMode: true,
-								icon: "warning",
-								buttons: ["No", "Si"],
-							}).then((respuesta) => {
-								if (respuesta) {
-									RemovalCamada({ uid: item.uid, uidMother: item.uidMother });
-								}
-							});
-						}}
-					/>
-				</div>
-			</div>
-			<br />
+    const cm = (
+        <div className={st.container}>
+            <div className={st.btnEdit}>
+                <div>
+                    <Buttons
+                        route='/formEditRepro'
+                        label='Editar'
+                        direction='bottom'
+                        btnIconText={faPenToSquare}
+                        btnClick={() => {
+                            recuperarCamada(item.uid);
+                        }}
+                    />
+                </div>
+                <div>
+                    <Buttons
+                        route='#'
+                        label='Borrar'
+                        direction='bottom'
+                        btnIconText={faTrash}
+                        btnClick={() => {
+                            swal({
+                                title: '¿Esta seguro que desea eliminar esta cuenta?',
+                                text: 'Una vez eliminada no podrá recuperarla',
+                                dangerMode: true,
+                                icon: 'warning',
+                                buttons: ['No', 'Si'],
+                            }).then((respuesta) => {
+                                if (respuesta) {
+                                    RemovalCamada({ uid: item.uid, uidMother: item.uidMother });
+                                }
+                            });
+                        }}
+                    />
+                </div>
+            </div>
+            <br />
 
-			<div className={st.panelInfo}>
-				<div className={st.pi}>
-					{stages?.map((element, index) => {
-						return (
-							<div className={st.panelcontainer}>
-								<table className={st.tableOut}>
-									<thead>
-										<tr>
-											<th>{element.title}</th>
-										</tr>
-									</thead>
-									<Ref key={index} stage={element} />
-								</table>
-							</div>
-						);
-					})}
-				</div>
-			</div>
-			<br />
-			<br />
-		</div>
-	);
-	return cm;
+            <div className={st.panelInfo}>
+                <div className={st.pi}>
+                    {stages?.map((element, index) => {
+                        return (
+                            <div className={st.panelcontainer}>
+                                <table className={st.tableOut}>
+                                    <thead>
+                                        <tr>
+                                            <th>{element.title}</th>
+                                        </tr>
+                                    </thead>
+                                    <Ref key={index} stage={element} />
+                                </table>
+                            </div>
+                        );
+                    })}
+                </div>
+            </div>
+            <br />
+            <br />
+        </div>
+    );
+    return cm;
 }
--- src/components/use/C_DataView/components/Vitae/components/ReproductiveCycle/components/Cards/components/Ref.jsx
+++ src/components/use/C_DataView/components/Vitae/components/ReproductiveCycle/components/Cards/components/Ref.jsx
@@ -1,123 +1,124 @@
-import st from "./Ref.module.css";
+import st from './Ref.module.css';
 
 export function Ref({ stage }) {
-	switch (stage.title) {
-		case "Monta":
-			return (
-				<tbody>
-					<table>
-						<tr>
-							<th>FECHA</th>
-							<td>{stage.date}</td>
-						</tr>
-						<tr>
-							<th>MACHO</th>
-							<td>{stage.male}</td>
-						</tr>
-					</table>
-				</tbody>
-			);
+    switch (stage.title) {
+        case 'Monta':
+            return (
+                <tbody>
+                    <table>
+                        <tr>
+                            <th>FECHA</th>
+                            <td>{stage.date}</td>
+                        </tr>
+                        <tr>
+                            <th>MACHO</th>
+                            <td>{stage.male}</td>
+                        </tr>
+                    </table>
+                </tbody>
+            );
 
-		case "Palpación":
-			return (
-				<tbody>
-					<table>
-						<tr>
-							<th>FECHA</th>
-							<td>{stage.date}</td>
-						</tr>
-					</table>
-				</tbody>
-			);
+        case 'Palpación':
+            return (
+                <tbody>
+                    <table>
+                        <tr>
+                            <th>FECHA</th>
+                            <td>{stage.date}</td>
+                        </tr>
+                    </table>
+                </tbody>
+            );
 
-		case "Preparto":
-			return (
-				<tbody>
-					<table>
-						<tr>
-							<th>FECHA</th>
-							<td>{stage.date}</td>
-						</tr>
-					</table>
-				</tbody>
-			);
+        case 'Preparto':
+            return (
+                <tbody>
+                    <table>
+                        <tr>
+                            <th>FECHA</th>
+                            <td>{stage.date}</td>
+                        </tr>
+                        <tr></tr>
+                    </table>
+                </tbody>
+            );
 
-		case "Parto":
-			return (
-				<tbody>
-					<table>
-						<tr>
-							<th>FECHA</th>
-							<td>{stage.date}</td>
-						</tr>
-						<tr>
-							<th>CRIAS</th>
+        case 'Parto':
+            return (
+                <tbody>
+                    <table>
+                        <tr>
+                            <th>FECHA</th>
+                            <td>{stage.date}</td>
+                        </tr>
+                        <tr>
+                            <th>CRIAS</th>
 
-							<td id={st.ask}>
-								<table className={st.tableIn}>
-									<thead></thead>
-									<tbody>
-										<tr>
-											<th>VIVOS</th>
-											<td>{stage.lives}</td>
-										</tr>
-										<tr>
-											<th>MUERTOS</th>
-											<td>{stage.deaths}</td>
-										</tr>
-										<tr>
-											<th>HOMOG.</th>
-											<td>{stage.homo}</td>
-										</tr>
-										<tr>
-											<th>TOTAL</th>
-											<td>{stage.total}</td>
-										</tr>
-									</tbody>
-								</table>
-							</td>
-						</tr>
-					</table>
-				</tbody>
-			);
+                            <td id={st.ask}>
+                                <table className={st.tableIn}>
+                                    <thead></thead>
+                                    <tbody>
+                                        <tr>
+                                            <th>VIVOS</th>
+                                            <td>{stage.lives}</td>
+                                        </tr>
+                                        <tr>
+                                            <th>MUERTOS</th>
+                                            <td>{stage.deaths}</td>
+                                        </tr>
+                                        <tr>
+                                            <th>HOMOG.</th>
+                                            <td>{stage.homo}</td>
+                                        </tr>
+                                        <tr>
+                                            <th>TOTAL</th>
+                                            <td>{stage.total}</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </td>
+                        </tr>
+                    </table>
+                </tbody>
+            );
 
-		case "Destete":
-			return (
-				<tbody>
-					<table>
-						<tr>
-							<th>FECHA</th>
-							<td>{stage.date}</td>
-						</tr>
+        case 'Destete':
+            return (
+                <tbody>
+                    <table>
+                        <tr>
+                            <th>FECHA</th>
+                            <td>{stage.date}</td>
+                        </tr>
 
-						<tr>
-							<th>CRIAS</th>
+                        <tr>
+                            <th>CRIAS</th>
 
-							<td id={st.ask}>
-								<table className={st.tableIn}>
-									<thead></thead>
-									<tbody>
-										<tr>
-											<th>MACHOS</th>
-											<td>{stage.MaleHatchlings}</td>
-										</tr>
-										<tr>
-											<th>HEMBRAS</th>
-											<td>{stage.FemaleHatchlings}</td>
-										</tr>
-										<tr>
-											<th>DESTETADOS</th>
-											<td>{stage.WeanedPups}</td>
-										</tr>
-									</tbody>
-								</table>
-							</td>
-						</tr>
-					</table>
-				</tbody>
-			);
+                            <td id={st.ask}>
+                                <table className={st.tableIn}>
+                                    <thead></thead>
+                                    <tbody>
+                                        <tr>
+                                            <th>MACHOS</th>
+                                            <td>{stage.MaleHatchlings}</td>
+                                        </tr>
+                                        <tr>
+                                            <th>HEMBRAS</th>
+                                            <td>{stage.FemaleHatchlings}</td>
+                                        </tr>
+                                        <tr>
+                                            <th>DESTETADOS</th>
+                                            <td>{stage.WeanedPups}</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </td>
+                        </tr>
+                    </table>
+                </tbody>
+            );
 
-		default:
-			return <></>;
-	}
+        default:
+            return <></>;
+    }
 }
