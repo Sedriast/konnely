85079f500fb772b5e05f3ea1e637d8a3b78fffc7
exSED <sedriast@gmail.com>
2022-11-29 12:56:37 -0500

    Rabbit List Styles RR

--- src/components/use/B_VitaeIsList/VitaeIsList.jsx
+++ src/components/use/B_VitaeIsList/VitaeIsList.jsx
@@ -1,9 +1,6 @@
-import st from "./VitaeIsList.module.css";
+import st from "../0-GeneralComp/OpContainers.module.css";
 
-import {
-  viewsListOptions,
-  generalOptions,
-} from "../0-GeneralComp/0-StaticData/options";
+import { viewsListOptions, generalOptions } from "../0-GeneralComp/0-StaticData/options";
 
 import { Option } from "./components/Option";
 import { useState } from "react";
@@ -11,23 +8,23 @@ import { RigthTopButtons } from "../0-GeneralComp/1-PanelButtons/RigthTopButtons
 import { LeftBottomMenu } from "../0-GeneralComp/1-PanelButtons/LeftBottomMenu/LeftBottomMenu";
 
 export function ViewIsList() {
-  const [optionSelect, setOptionSelect] = useState(false);
+	const [optionSelect, setOptionSelect] = useState(false);
 
-  const cm = (
-    <>
-      <LeftBottomMenu
-        backCancel={optionSelect ? viewsListOptions[0] : null}
-        additionExtras={!optionSelect ? viewsListOptions[1] : null}
-        click={() => {
-          setOptionSelect(!optionSelect);
-        }}
-      />
-      <div className={st.optionContainer}>
-        <Option op={optionSelect} />
-      </div>
-      {optionSelect ? <></> : <RigthTopButtons BTNS={generalOptions} />}
-    </>
-  );
+	const cm = (
+		<>
+			<LeftBottomMenu
+				backCancel={optionSelect ? viewsListOptions[0] : null}
+				additionExtras={!optionSelect ? viewsListOptions[1] : null}
+				click={() => {
+					setOptionSelect(!optionSelect);
+				}}
+			/>
+			<div className={st.optionContainer}>
+				<Option op={optionSelect} />
+			</div>
+			{optionSelect ? <></> : <RigthTopButtons BTNS={generalOptions} />}
+		</>
+	);
 
-  return cm;
+	return cm;
 }
