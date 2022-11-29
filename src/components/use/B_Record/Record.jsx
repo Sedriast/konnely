6f6697a0f5cf730571b24956403d91bd8541b104
exSED <sedriast@gmail.com>
2022-11-29 15:39:00 -0500

    Styles

--- src/components/use/B_Record/Record.jsx
+++ src/components/use/B_Record/Record.jsx
@@ -1,5 +1,16 @@
-import st from "./Record.module.css";
+import st from "../0-GeneralComp/OpContainers.module.css";
+
+import { RigthTopButtons } from "../0-GeneralComp/1-PanelButtons/RigthTopButtons/RigthTopButtons";
+import { generalOptions } from "../0-GeneralComp/0-StaticData/options";
+import { ViewRecord } from "./components/ViewRecord";
 
 export function Record() {
-	return <></>;
+	return (
+		<>
+			<div className={st.optionContainer}>
+				<ViewRecord />
+			</div>
+			<RigthTopButtons BTNS={generalOptions} />
+		</>
+	);
 }
---
+++ src/components/use/B_Record/components/ViewRecord.jsx
@@ -0,0 +1,73 @@
+import st from "./ViewRecord.module.css";
+
+import { useState } from "react";
+
+import { faFileCirclePlus, faXmark, faCheck } from "@fortawesome/free-solid-svg-icons";
+
+import { Buttons } from "../../0-GeneralComp/1-Buttons/Buttons";
+import { basicData } from "../../0-GeneralComp/0-StaticData/dataProv";
+import { QueriesSimple_ } from "../../../firebase/funtions/GetInformation";
+
+export function ViewRecord() {
+	let activos = [];
+	let inactivos = [];
+	const [cam, setCam] = useState(true);
+	const [search, setSearch] = useState("");
+	const [search_, setSearch_] = useState([""]);
+	const trataments = QueriesSimple_({
+		coleccion: "trataments",
+		parametro: "uidRabbit",
+		busqueda: basicData?.info?.uid,
+	}).props.children;
+	trataments.map((items) => {
+		if (items.state === "Activo") {
+			return activos.push(items);
+		} else if (items.state === "Inactivo") {
+			return inactivos.push(items);
+		}
+		return null;
+	});
+
+	function buscar(e) {
+		setSearch(e);
+		const valor = trataments.filter(function (element) {
+			if (
+				element.signs.toString().toLowerCase().includes(e.toLowerCase()) ||
+				element.date.toString().toLowerCase().includes(e.toLowerCase()) ||
+				element.diagnosis.toString().toLowerCase().includes(e.toLowerCase())
+			)
+				return element;
+			return null;
+		});
+		setSearch_(valor);
+	}
+
+	return (
+		<div className={st.container}>
+			<div className={st.panelSearchBar}>
+				<div className={st.new}>
+					<Buttons
+						btnIconText={faFileCirclePlus}
+						route='/addHis'
+						label='Nuevo registro'
+						direction='rigth'
+					/>
+				</div>
+				<form>
+					<input
+						name='buscar'
+						type='search'
+						placeholder='Buscar'
+						onChange={(e) => {
+							buscar(e.target.value);
+						}}
+					/>
+					<button className={st.btnSearch} type='submit'>
+						🔎
+					</button>
+				</form>
+			</div>
+			<div className={st.panelItems}></div>
+		</div>
+	);
+}
