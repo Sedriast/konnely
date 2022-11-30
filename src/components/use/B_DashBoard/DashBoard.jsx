cd0473de8a268d936b33e4035e752838e4936758
exSED <sedriast@gmail.com>
2022-11-29 20:34:13 -0500

    styles

--- src/components/use/B_DashBoard/DashBoard.jsx
+++ src/components/use/B_DashBoard/DashBoard.jsx
@@ -1,15 +1,17 @@
-import st from './DashBoard.module.css';
+import st from "./DashBoard.module.css";
 
-import { RigthTopButtons } from '../0-GeneralComp/1-PanelButtons/RigthTopButtons/RigthTopButtons';
-import { generalOptions } from '../0-GeneralComp/0-StaticData/options';
-import { List } from './component/List/List';
-import { PanelData } from './component/PanelData';
+import { RigthTopButtons } from "../0-GeneralComp/1-PanelButtons/RigthTopButtons/RigthTopButtons";
+import { generalOptions } from "../0-GeneralComp/0-StaticData/options";
+import { List } from "./component/List/List";
+import { PanelData } from "./component/PanelData";
+import { Loading } from "../0-GeneralComp/1-Loading/Loading";
 
 export function DashBoard() {
-    return (
-        <>
-            <div className={st.optionContainer}>
-                <div className={st.panelA}>
+	return (
+		<>
+			<div className={st.optionContainer}>
+				<Loading />
+				{/* <div className={st.panelA}>
                     <div className={st.one}>
                         <PanelData />
                     </div>
@@ -19,9 +21,9 @@ export function DashBoard() {
                 </div>
                 <div className={st.panelB}>
                     <List />
-                </div>
-            </div>
-            <RigthTopButtons BTNS={generalOptions} />
-        </>
-    );
+                </div> */}
+			</div>
+			<RigthTopButtons BTNS={generalOptions} />
+		</>
+	);
 }
