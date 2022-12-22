8801897d2de1632e180ae516b2d8faa3578d1117
Sedriast <sedriast@gmail.com>
2022-12-22 13:52:26 -0500

    STYLE: Search bar

--- src/components/use/0-GeneralComp/1-EditHistory/EditHistory.jsx
+++ src/components/use/0-GeneralComp/1-EditHistory/EditHistory.jsx
@@ -1,4 +1,5 @@
 import st from '../OpContainers.module.css';
+import st_ from '../../A_User/components/ListUsers/ListUsers.module.css';
 
 import { newTreats } from '../0-StaticData/options';
 import { LeftBottomMenu } from '../1-PanelButtons/LeftBottomMenu/LeftBottomMenu';
@@ -13,6 +14,19 @@ export function EditHistory({userList}) {
 				}}
 			/>
             <div className={st.optionContainer}>
+                <div className={st_.panelSearchBar}>
+                    <form>
+                        <input
+                            name='buscar'
+                            placeholder='Buscar'
+                            type='search'
+                            onChange={(e) => {
+                                //buscar(e.target.value);
+                            }}
+                        />
+                        <button className={st_.btnSearch}>🔎</button>
+                    </form>
+                </div>
                 {userList?.map((user, i) => {
                     <EditUserCard key={i} user={user}/>
                 })}
