e14999c8414186dca9841306e41fc8b7de6e2196
SexDEG <alonsofelipe8888@gmail.com>
2023-02-15 10:21:22 -0500

    ci(Seguridad): :ambulance: update of the security

--- src/components/firebase/credentials.js
+++ src/components/firebase/credentials.js
@@ -1,13 +1,14 @@
 import { initializeApp } from 'firebase/app';
+require('dotenv').config();
 
 const firebaseConfig = {
-    apiKey: 'AIzaSyDEkudVm0yVDUoxjahRNWTDOVBumd1CJvQ',
-    authDomain: 'konnely-e4041.firebaseapp.com',
-    projectId: 'konnely-e4041',
-    storageBucket: 'konnely-e4041.appspot.com',
-    messagingSenderId: '1041352313579',
-    appId: '1:1041352313579:web:6c0541ab544a5bd3b38cae',
-    measurementId: 'G-7S34NTVHPL',
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
